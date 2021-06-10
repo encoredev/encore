d3ae7503f82089daeb60d0c664ae0ff8a8d25a31
Will Demaine <willdemaine@gmail.com>
2021-06-10 14:34:19 +0100

    Hook up router

--- compiler/runtime/runtime/setup.go
+++ compiler/runtime/runtime/setup.go
@@ -5,24 +5,33 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
 
 	"encore.dev/runtime/config"
 	"github.com/hashicorp/yamux"
+	"github.com/julienschmidt/httprouter"
 	"github.com/rs/zerolog"
 )
 
 type Server struct {
-	logger   zerolog.Logger
-	handlers map[string]*config.Endpoint
+	logger zerolog.Logger
+	router *httprouter.Router
 }
 
 func (srv *Server) handleRPC(service string, endpoint *config.Endpoint) {
-	srv.logger.Info().Str("service", service).Str("endpoint", endpoint.Name).Msg("registered endpoint")
-	key := service + "." + endpoint.Name
-	srv.handlers[key] = endpoint
+	logMsg := srv.logger.Info().Str("service", service).Str("endpoint", endpoint.Name)
+	if endpoint.Path != "" {
+		logMsg.Str("path", endpoint.Path)
+	}
+	logMsg.Msg("registered endpoint")
+
+	key := fmt.Sprintf("/%s.%s", service, endpoint.Name)
+	srv.router.Handle("*", filepath.Join(key, endpoint.Path), func(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+		endpoint.Handler(w, req)
+	})
 }
 
 func (srv *Server) ListenAndServe() error {
@@ -71,12 +80,12 @@ func (srv *Server) setupConn() (io.ReadWriteCloser, error) {
 }
 
 func (srv *Server) handler(w http.ResponseWriter, req *http.Request) {
-	endpoint := srv.handlers[strings.TrimPrefix(req.URL.Path, "/")]
-	if endpoint == nil {
+	handler, _, ok := srv.router.Lookup("*", req.URL.Path)
+	if !ok {
 		http.Error(w, "Endpoint Not Found", http.StatusNotFound)
 		return
 	}
-	endpoint.Handler(w, req)
+	handler(w, req, nil)
 }
 
 func Setup(cfg *config.ServerConfig) *Server {
@@ -85,8 +94,8 @@ func Setup(cfg *config.ServerConfig) *Server {
 	Config = cfg
 
 	srv := &Server{
-		logger:   logger,
-		handlers: make(map[string]*config.Endpoint),
+		logger: logger,
+		router: httprouter.New(),
 	}
 	for _, svc := range cfg.Services {
 		for _, endpoint := range svc.Endpoints {
