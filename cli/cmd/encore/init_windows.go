75a63f11225e8063327c8da14df5a280fadcafa5
Farzam Alam <farzamcse@gmail.com>
2021-08-15 17:51:04 +0530

    cli/encore: set console mode for Windows
    Set console mode for stdout and stderr in windows in order to properly handle ANSI terminal codes.
    
    Fixes #56

---
+++ cli/cmd/encore/init_windows.go
@@ -0,0 +1,22 @@
+// +build windows
+
+package main
+
+import (
+	"golang.org/x/sys/windows"
+)
+
+// init activates virtual terminal feature on "windows", this enables colored
+// terminal output.
+func init() {
+	setConsoleMode(windows.Stdout, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	setConsoleMode(windows.Stderr, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
+
+// setConsoleMode enables VT processing on stout and stderr.
+func setConsoleMode(handle windows.Handle, flag uint32) {
+	var mode uint32
+	if err := windows.GetConsoleMode(handle, &mode); err == nil {
+		windows.SetConsoleMode(handle, mode|flag)
+	}
+}
