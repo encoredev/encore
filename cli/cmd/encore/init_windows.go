4beb24a4c757b1471ca55da67c901c61919a9046
farzamalam <farzamcse@gmail.com>
2021-08-15 16:20:14 +0530

    Set console mode for stdout and stderr in windows. Fixes #56

---
+++ cli/cmd/encore/init_windows.go
@@ -0,0 +1,21 @@
+// +build windows
+
+package main
+
+import (
+	"golang.org/x/sys/windows"
+)
+
+// init activates virtual terminal feature on "windows", this enables colored
+// terminal output.
+func init() {
+	setConsoleMode(windows.Stdout, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	setConsoleMode(windows.Stderr, windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
+
+// setConsoleMode enables VT processing on stout and stderr.
+func setConsoleMode(handle windows.Handle, flag uint32) {
+	var mode uint32
+	windows.GetConsoleMode(handle, &mode)
+	windows.SetConsoleMode(handle, mode|flag)
+}
