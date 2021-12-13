22e86fb3da8b1660934668438c5ec66c462e45a6
André Eriksson <andre@encore.dev>
2021-12-13 12:56:56 +0100

    add ability to override config dir

--- cli/internal/version/version.go
+++ cli/internal/version/version.go
@@ -1,5 +1,28 @@
 package version
 
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"os"
+)
+
 // Version is the version of the encore binary.
 // It is set using `go build -ldflags "-X encr.dev/cli/internal/version.Version=v1.2.3"`.
 var Version string
+
+// ConfigHash reports a hash of the configuration that affects the behavior of the daemon.
+// It is used to decide whether to restart the daemon.
+func ConfigHash() string {
+	envs := []string{
+		"ENCORE_DAEMON_DEV",
+		"ENCORE_PLATFORM_API_URL",
+		"ENCORE_CONFIG_DIR",
+	}
+	h := sha256.New()
+	for _, e := range envs {
+		fmt.Fprintf(h, "%s=%q\n", e, os.Getenv(e))
+	}
+	digest := h.Sum(nil)
+	return base64.RawURLEncoding.EncodeToString(digest)
+}
--- proto/gen.sh
+++ proto/gen.sh
@@ -5,7 +5,4 @@ set -e -x
 protoc -I . --go_out=. --go_opt=paths=source_relative ./encore/parser/meta/v1/meta.proto
 protoc -I . --go_out=. --go_opt=paths=source_relative ./encore/parser/schema/v1/schema.proto
 protoc -I . --go_out=. --go_opt=paths=source_relative ./encore/engine/trace/trace.proto
-
 protoc -I . --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./encore/daemon/daemon.proto
-protoc -I . --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./encore/server/remote/remote.proto
-protoc -I . --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./encore/server/remote/tunnel/tunnel.proto
