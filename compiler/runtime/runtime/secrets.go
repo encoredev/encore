5f75dfe1abe2e5b421425edb54dc5f966b259afa
André Eriksson <andre@encore.dev>
2022-02-08 16:05:34 +0100

    compiler/runtime/runtime/config: fix secrets loading
    Load secrets using the new config mechanism.

--- compiler/runtime/runtime/secrets.go
+++ compiler/runtime/runtime/secrets.go
@@ -1,47 +1,17 @@
 package runtime
 
 import (
-	"encoding/base64"
 	"fmt"
 	"os"
-	"strings"
+
+	"encore.dev/runtime/config"
 )
 
 func LoadSecret(key string) string {
-	val, ok := secrets[key]
+	val, ok := config.Cfg.Secrets[key]
 	if !ok {
 		fmt.Fprintln(os.Stderr, "encore: could not find secret", key)
 		os.Exit(2)
 	}
 	return val
-}
-
-var secrets = loadSecrets()
-
-func loadSecrets() map[string]string {
-	const env = "ENCORE_SECRETS"
-	encoded := os.Getenv(env)
-	os.Unsetenv(env)
-	if encoded == "" {
-		return nil
-	}
-
-	// Format is "key1=val1,key2=val2" where values are base64-encoded using RawStdEncoding.
-	secrets := make(map[string]string)
-	fields := strings.Split(encoded, ",")
-	for _, f := range fields {
-		eql := strings.IndexByte(f, '=')
-		if eql == -1 {
-			fmt.Fprintln(os.Stderr, "encore: internal error: invalid ENCORE_SECRETS format")
-			os.Exit(2)
-		}
-		key := f[:eql]
-		value, err := base64.RawStdEncoding.DecodeString(f[eql+1:])
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "encore: internal error: invalid ENCORE_SECRETS format")
-			os.Exit(2)
-		}
-		secrets[key] = string(value)
-	}
-	return secrets
 }
