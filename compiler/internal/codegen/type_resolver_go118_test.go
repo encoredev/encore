e2ad9c94b6d83cc0aeca0cfc21c9785f24ec7adb
Dominic Black <dom@encore.dev>
2022-02-09 13:20:27 +0000

    Disable generic tests when not Go 1.18 (#119)
    This commit uses Go build tags to disable two tests which now
    rely on Go 1.18 generics which are causing test failures when we
    test on Go 1.17 or below.

---
+++ compiler/internal/codegen/type_resolver_go118_test.go
@@ -0,0 +1,11 @@
+//go:build go1.18
+
+package codegen
+
+import . "encr.dev/proto/encore/parser/schema/v1"
+
+func init() {
+	schemaTypeToGoTypeTestCases = append(schemaTypeToGoTypeTestCases, schemaTypeToGoTypeTestCase{
+		"generic named types", &Type{Typ: &Type_Named{Named: &Named{Id: 0, TypeArguments: []*Type{intType, uuidType}}}}, "codegentest.UserAge[int, uuid.UUID]",
+	})
+}
