32dc8333a6ea7c1a665b0e5f987edd644a0da7a7
André Eriksson <andre@encore.dev>
2022-05-07 10:37:36 +0200

    runtime: separate sql server config from databases
    It's common to use the same database server with multiple databases.
    In combination with client side TLS certificates this creates an
    enormous amount of duplicated data, to the point where the size of the
    encoded config can exceed the 64KiB limits imposed by Cloud Run.
    
    Fix this by separating out the SQL Server configuration from the
    SQL Database configuration, so that multiple databases on the same
    server can use the same TLS configuration.

--- cli/daemon/runtime/config/config.go
+++ cli/daemon/runtime/config/config.go
@@ -9,6 +9,7 @@ type Runtime struct {
 	TraceEndpoint string          `json:"trace_endpoint"`
 	AuthKeys      []EncoreAuthKey `json:"auth_keys"`
 	SQLDatabases  []*SQLDatabase  `json:"sql_databases"`
+	SQLServers    []*SQLServer    `json:"sql_servers"`
 }
 
 type EncoreAuthKey struct {
@@ -16,10 +17,31 @@ type EncoreAuthKey struct {
 	Data  []byte `json:"data"`
 }
 
+type SQLServer struct {
+	// Host is the host to connect to.
+	// Valid formats are "hostname", "hostname:port", and "/path/to/unix.socket".
+	Host string `json:"host"`
+
+	// MinConnections is the minimum number of open connections to use
+	// for this database. If zero it defaults to 2.
+	MinConnections int `json:"min_connections"`
+
+	// MaxConnections is the maximum number of open connections to use
+	// for this database. If zero it defaults to 30.
+	MaxConnections int `json:"max_connections"`
+
+	// ServerCACert is the PEM-encoded server CA cert, or "" if not required.
+	ServerCACert string `json:"server_ca_cert"`
+	// ClientCert is the PEM-encoded client cert, or "" if not required.
+	ClientCert string `json:"client_cert"`
+	// ClientKey is the PEM-encoded client key, or "" if not required.
+	ClientKey string `json:"client_key"`
+}
+
 type SQLDatabase struct {
+	ServerID     int    `json:"server_id"`     // the index into (*Runtime).SQLServers
 	EncoreName   string `json:"encore_name"`   // the Encore name for the database
 	DatabaseName string `json:"database_name"` // the actual database name as known by the SQL server.
-	Host         string `json:"host"`
 	User         string `json:"user"`
 	Password     string `json:"password"`
 }
