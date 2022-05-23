98cfe24c62c227d22337a4ab9f2187a0ae008e66
Dominic Black <dom@encore.dev>
2022-05-23 12:27:13 +0100

    Add structured errors to Typescript Client (#224)
    This commit adds structured errors to the typescript client in the
    form of an `EncoreError` object which will be thrown incase of an
    error. This makes a best attempt at recovering the structured error
    as reported by an Encore API and parsing it to be easily usable from
    withing a client.
    
    This also causes the client to premptively return throw an error if
    no bearer token is present when trying to call an Authenticated API.
    
    Fixes #114
    
    ```typescript
    const client = new Client(Local)
    
    try {
        const productListing = await client.products.List()
    } catch (e: Error) {
        if (isEncoreError(e)) {
            console.log(`Got an ${e.code} error with message ${e.message} and details ${e.details}`)
        } else {
           console.log(`Got error ${e.message}`)
        }
    }
    ```

--- cli/internal/codegen/typescript.go
+++ cli/internal/codegen/typescript.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"reflect"
 	"sort"
+	"strconv"
 	"strings"
 	"unicode"
 
@@ -88,7 +89,7 @@ func (ts *typescript) Generate(buf *bytes.Buffer, appSlug string, md *meta.Data)
 	}
 	ts.writeExtraTypes()
 	ts.writeBaseClient(appSlug)
-	ts.writeQueryStringHelper()
+	ts.writeCustomErrorType()
 
 	return nil
 }
@@ -253,9 +254,10 @@ func (ts *typescript) rpcCallSite(ns string, w *indentWriter, rpc *meta.RPC, rpc
 	// and need no further code generation
 	if rpc.Proto == meta.RPC_RAW {
 		w.WriteStringf(
-			"return this.baseClient.callAPI(\"%s\", `%s`, body, options)\n",
+			"return this.baseClient.callAPI(\"%s\", `%s`, %s, body, options)\n",
 			rpcEncoding.DefaultRequestEncoding.HTTPMethods[0],
 			rpcPath,
+			strconv.FormatBool(rpc.AccessType == meta.RPC_AUTH),
 		)
 		return nil
 	}
@@ -328,9 +330,10 @@ func (ts *typescript) rpcCallSite(ns string, w *indentWriter, rpc *meta.RPC, rpc
 
 	// Build the call to callAPI
 	callAPI := fmt.Sprintf(
-		"this.baseClient.callAPI(\"%s\", `%s`",
+		"this.baseClient.callAPI(\"%s\", `%s`, %s",
 		rpcEncoding.DefaultRequestEncoding.HTTPMethods[0],
 		rpcPath,
+		strconv.FormatBool(rpc.AccessType == meta.RPC_AUTH),
 	)
 	if body != "" || headers != "" {
 		if body == "" {
@@ -588,7 +591,7 @@ class BaseClient {
     }
 
     // callAPI is used by each generated API method to actually make the request
-    public async callAPI(method: string, path: string, body?: BodyInit, params?: CallParameters): Promise<Response> {
+    public async callAPI(method: string, path: string, requiresAuth: boolean, body?: BodyInit, params?: CallParameters): Promise<Response> {
         const init: RequestInit = {
             method,
             body,
@@ -598,9 +601,19 @@ class BaseClient {
         // Merge our headers with any predefined headers
         init.headers = {...this.headers, ...init.headers}
 
+        let bearerToken: string | undefined
+
         // If an authorization token generator is present, call it and add the returned token to the request
         if (this.tokenGenerator) {
-            init.headers["Authorization"] = "Bearer " + this.tokenGenerator()
+            bearerToken = this.tokenGenerator()
+        }
+
+        // If we now have a bearer token, add it to the request
+        if (bearerToken) {
+            init.headers["Authorization"] = "Bearer " + bearerToken
+        } else if (requiresAuth) {
+            // The API called requires authorization, but we don't have a token generator, or it provided a blank token value
+            throw new APIError(400, { code: ErrCode.Unauthenticated, message: 'Authorization token required for this API, but none provided.' })
         }
 
         // Make the actual request
@@ -608,8 +621,29 @@ class BaseClient {
 
         // handle any error responses
         if (!response.ok) {
-            const body = await response.text()
-            throw new Error(` + "`request failed: status ${response.status}: ${body}`" + `)
+            // try and get the error message from the response body
+            let body: APIErrorResponse = { code: ErrCode.Unknown, message: ` + "`request failed: status ${response.status}`" + ` }
+
+            // if we can get the structured error we should, otherwise give a best effort
+            try {
+                const text = await response.text()
+
+                try {
+                    const jsonBody = JSON.parse(text)
+                    if (isAPIErrorResponse(jsonBody)) {
+                        body = jsonBody
+                    } else {
+                        body.message += ": " + JSON.stringify(jsonBody)
+                    }
+                } catch {
+                    body.message += ": " + text
+                }
+            } catch (e) {
+                // otherwise we just append the text to the error message
+                body.message += ": " + e.toString()
+            }
+
+            throw new APIError(response.status, body)
         }
 
         return response
@@ -617,7 +651,13 @@ class BaseClient {
 }`)
 }
 
-func (ts *typescript) writeQueryStringHelper() {
+func (ts *typescript) writeExtraTypes() {
+	if ts.seenJSON {
+		ts.WriteString(`// JSONValue represents an arbitrary JSON value.
+export type JSONValue = string | number | boolean | null | JSONValue[] | {[key: string]: JSONValue}
+`)
+	}
+
 	if ts.seenQueryString {
 		ts.WriteString(`
 
@@ -634,14 +674,6 @@ function encodeQuery(parts: Record<string, any>): string {
     }
     return pairs.join("&")
 }`)
-	}
-}
-
-func (ts *typescript) writeExtraTypes() {
-	if ts.seenJSON {
-		ts.WriteString(`// JSONValue represents an arbitrary JSON value.
-export type JSONValue = string | number | boolean | null | JSONValue[] | {[key: string]: JSONValue}
-`)
 	}
 }
 
@@ -915,4 +947,290 @@ func (ts *typescript) fieldNameInStruct(field *schema.Field) string {
 		name = field.JsonName
 	}
 	return name
+}
+
+func (ts *typescript) writeCustomErrorType() {
+	w := ts.newIdentWriter(0)
+
+	w.WriteString(`
+
+/**
+ * APIErrorDetails represents the response from an Encore API in the case of an error
+ */
+interface APIErrorResponse {
+    code: ErrCode
+    message: string
+    details?: any
+}
+
+function isAPIErrorResponse(err: any): err is APIErrorResponse {
+    return (
+        err !== undefined && err !== null && 
+        typeof(err.code) === "number" &&
+        typeof(err.message) === "string" &&
+        (err.details === undefined || err.details === null || typeof(err.details) === "object")
+    )
+}
+
+/**
+ * APIError represents a structured error as returned from an Encore application.
+ */
+export class APIError extends Error {
+    /**
+     * The name of this error class
+     */
+    public readonly name: 'APIError'
+
+    /**
+     * The HTTP status code associated with the error.
+     */
+    public readonly status: number
+
+    /**
+     * The Encore error code
+     */
+    public readonly code: ErrCode
+
+    /**
+     * The error message
+     */
+    public readonly message: string
+
+    /**
+     * The error details
+     */
+    public readonly details?: any
+
+    constructor(status: number, response: APIErrorResponse) {
+        // extending errors causes issues after you construct them, unless you apply the following fixes
+        super(response.message);
+        
+        // set error name as constructor name, make it not enumerable to keep native Error behavior
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new.target#new.target_in_constructors
+        Object.defineProperty(this, 'name', {
+            value:        'APIError',
+            enumerable:   false,
+            configurable: true,
+        })
+        
+        // fix the prototype chain
+        if ((Object as any).setPrototypeOf == undefined) { 
+            (this as any).__proto__ = APIError.prototype 
+        } else {
+            Object.setPrototypeOf(this, APIError.prototype);
+        }
+        
+        // capture a stack trace
+        if ((Error as any).captureStackTrace !== undefined) {
+            (Error as any).captureStackTrace(this, this.constructor);
+        }
+
+        this.status = status
+        this.code = response.code
+        this.details = response.details
+    }
+}
+
+/**
+ * Typeguard allowing use of an APIError's fields'
+ */
+export function isAPIError(err: any): err is APIError {
+    return err instanceof APIError;
+}
+
+export enum ErrCode {
+    /**
+     * OK indicates the operation was successful.
+     */
+    OK = "ok",
+
+    /**
+     * Canceled indicates the operation was canceled (typically by the caller).
+     *
+     * Encore will generate this error code when cancellation is requested.
+     */
+    Canceled = "canceled",
+
+    /**
+     * Unknown error. An example of where this error may be returned is
+     * if a Status value received from another address space belongs to
+     * an error-space that is not known in this address space. Also
+     * errors raised by APIs that do not return enough error information
+     * may be converted to this error.
+     *
+     * Encore will generate this error code in the above two mentioned cases.
+     */
+    Unknown = "unknown",
+
+    /**
+     * InvalidArgument indicates client specified an invalid argument.
+     * Note that this differs from FailedPrecondition. It indicates arguments
+     * that are problematic regardless of the state of the system
+     * (e.g., a malformed file name).
+     *
+     * This error code will not be generated by the gRPC framework.
+     */
+    InvalidArgument = "invalid_argument",
+
+    /**
+     * DeadlineExceeded means operation expired before completion.
+     * For operations that change the state of the system, this error may be
+     * returned even if the operation has completed successfully. For
+     * example, a successful response from a server could have been delayed
+     * long enough for the deadline to expire.
+     *
+     * The gRPC framework will generate this error code when the deadline is
+     * exceeded.
+     */
+    DeadlineExceeded = "deadline_exceeded",
+
+    /**
+     * NotFound means some requested entity (e.g., file or directory) was
+     * not found.
+     *
+     * This error code will not be generated by the gRPC framework.
+     */
+    NotFound = "not_found",
+
+    /**
+     * AlreadyExists means an attempt to create an entity failed because one
+     * already exists.
+     *
+     * This error code will not be generated by the gRPC framework.
+     */
+    AlreadyExists = "already_exists",
+
+    /**
+     * PermissionDenied indicates the caller does not have permission to
+     * execute the specified operation. It must not be used for rejections
+     * caused by exhausting some resource (use ResourceExhausted
+     * instead for those errors). It must not be
+     * used if the caller cannot be identified (use Unauthenticated
+     * instead for those errors).
+     *
+     * This error code will not be generated by the gRPC core framework,
+     * but expect authentication middleware to use it.
+     */
+    PermissionDenied = "permission_denied",
+
+    /**
+     * ResourceExhausted indicates some resource has been exhausted, perhaps
+     * a per-user quota, or perhaps the entire file system is out of space.
+     *
+     * This error code will be generated by the gRPC framework in
+     * out-of-memory and server overload situations, or when a message is
+     * larger than the configured maximum size.
+     */
+    ResourceExhausted = "resource_exhausted",
+
+    /**
+     * FailedPrecondition indicates operation was rejected because the
+     * system is not in a state required for the operation's execution.
+     * For example, directory to be deleted may be non-empty, an rmdir
+     * operation is applied to a non-directory, etc.
+     *
+     * A litmus test that may help a service implementor in deciding
+     * between FailedPrecondition, Aborted, and Unavailable:
+     *  (a) Use Unavailable if the client can retry just the failing call.
+     *  (b) Use Aborted if the client should retry at a higher-level
+     *      (e.g., restarting a read-modify-write sequence).
+     *  (c) Use FailedPrecondition if the client should not retry until
+     *      the system state has been explicitly fixed. E.g., if an "rmdir"
+     *      fails because the directory is non-empty, FailedPrecondition
+     *      should be returned since the client should not retry unless
+     *      they have first fixed up the directory by deleting files from it.
+     *  (d) Use FailedPrecondition if the client performs conditional
+     *      REST Get/Update/Delete on a resource and the resource on the
+     *      server does not match the condition. E.g., conflicting
+     *      read-modify-write on the same resource.
+     *
+     * This error code will not be generated by the gRPC framework.
+     */
+    FailedPrecondition = "failed_precondition",
+
+    /**
+     * Aborted indicates the operation was aborted, typically due to a
+     * concurrency issue like sequencer check failures, transaction aborts,
+     * etc.
+     *
+     * See litmus test above for deciding between FailedPrecondition,
+     * Aborted, and Unavailable.
+     */
+    Aborted = "aborted",
+
+    /**
+     * OutOfRange means operation was attempted past the valid range.
+     * E.g., seeking or reading past end of file.
+     *
+     * Unlike InvalidArgument, this error indicates a problem that may
+     * be fixed if the system state changes. For example, a 32-bit file
+     * system will generate InvalidArgument if asked to read at an
+     * offset that is not in the range [0,2^32-1], but it will generate
+     * OutOfRange if asked to read from an offset past the current
+     * file size.
+     *
+     * There is a fair bit of overlap between FailedPrecondition and
+     * OutOfRange. We recommend using OutOfRange (the more specific
+     * error) when it applies so that callers who are iterating through
+     * a space can easily look for an OutOfRange error to detect when
+     * they are done.
+     *
+     * This error code will not be generated by the gRPC framework.
+     */
+    OutOfRange = "out_of_range",
+
+    /**
+     * Unimplemented indicates operation is not implemented or not
+     * supported/enabled in this service.
+     *
+     * This error code will be generated by the gRPC framework. Most
+     * commonly, you will see this error code when a method implementation
+     * is missing on the server. It can also be generated for unknown
+     * compression algorithms or a disagreement as to whether an RPC should
+     * be streaming.
+     */
+    Unimplemented = "unimplemented",
+
+    /**
+     * Internal errors. Means some invariants expected by underlying
+     * system has been broken. If you see one of these errors,
+     * something is very broken.
+     *
+     * This error code will be generated by the gRPC framework in several
+     * internal error conditions.
+     */
+    Internal = "internal",
+
+    /**
+     * Unavailable indicates the service is currently unavailable.
+     * This is a most likely a transient condition and may be corrected
+     * by retrying with a backoff. Note that it is not always safe to retry
+     * non-idempotent operations.
+     *
+     * See litmus test above for deciding between FailedPrecondition,
+     * Aborted, and Unavailable.
+     *
+     * This error code will be generated by the gRPC framework during
+     * abrupt shutdown of a server process or network connection.
+     */
+    Unavailable = "unavailable",
+
+    /**
+     * DataLoss indicates unrecoverable data loss or corruption.
+     *
+     * This error code will not be generated by the gRPC framework.
+     */
+    DataLoss = "data_loss",
+
+    /**
+     * Unauthenticated indicates the request does not have valid
+     * authentication credentials for the operation.
+     *
+     * The gRPC framework will generate this error code when the
+     * authentication metadata is invalid or a Credentials callback fails,
+     * but also expect authentication middleware to generate it.
+     */
+    Unauthenticated = "unauthenticated",
+}
+`)
 }
