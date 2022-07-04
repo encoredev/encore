f4f0f9a06bb24b09b6627cfa12dfc56f87a785d4
Dominic Black <dom@encore.dev>
2022-07-04 10:23:29 +0000

    Fixed nil pointer on unbound resources. (#301)
    Most requires are bound to package level varaibles, like this:
    ```go
    var db = sqldb.Named("svc")
    ```
    
    We track the identifier `db` as being bound to the database `svc`.
    However, it's possible to make calls like this:
    
    ```go
    var db = sqldb.Named("svc").Stdlib()
    ```
    
    In this case `db` is not longer bound against the database `svc`, as
    the named resource isn't directly bound.
    
    The refactored resource parser, correctly identified the named database,
    and it wasn't bound to a varaible in the package. However, older code
    was written expecting an identified resource to always have an ident.
    
    This is what caused the nil pointer panic. I've added checks for `nil`
    identifiers, and skip the resource usage tracking if we know that resource
    isn't bound against anything.

--- parser/parser.go
+++ parser/parser.go
@@ -314,7 +314,9 @@ func (p *parser) parseReferences() {
 		resourceMap[pkg.ImportPath] = resources
 		for _, res := range pkg.Resources {
 			id := res.Ident()
-			resources[id.Name] = res
+			if id != nil {
+				resources[id.Name] = res
+			}
 		}
 	}
 
