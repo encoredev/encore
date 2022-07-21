7351e522ecda2a5248c8427f979a8e44ecddf71c
André Eriksson <andre@encore.dev>
2022-07-21 21:08:57 +0200

    codegen: simplify main, testmain
    This simplifies the Main and TestMain functions to use the new,
    simplified config loading and bootstrapping.

--- compiler/internal/codegen/codegen_testmain.go
+++ compiler/internal/codegen/codegen_testmain.go
@@ -19,41 +19,22 @@ func (b *Builder) TestMain(pkg *est.Package, svcs []*est.Service) *File {
 		f.ImportName(p.ImportPath, p.Name)
 	}
 
-	// Import the runtime package with '_' as its name to start with to ensure it's imported.
-	// If other code uses it it will be imported under its proper name.
-	f.Anon("encore.dev/runtime")
-
-	getEnv := func(name string) Code {
-		return Qual("os", "Getenv").Call(Lit(name))
-	}
-
 	f.Anon("unsafe") // for go:linkname
 	testSvc := ""
 	if pkg.Service != nil {
 		testSvc = pkg.Service.Name
 	}
-	f.Comment("//go:linkname loadConfig encore.dev/runtime/config.loadConfig")
-	f.Func().Id("loadConfig").Params().Params(Op("*").Qual("encore.dev/runtime/config", "Config"), Error()).Block(
-		Id("services").Op(":=").Index().Op("*").Qual("encore.dev/runtime/config", "Service").ValuesFunc(func(g *Group) {
-			for _, svc := range b.res.App.Services {
-				g.Values(Dict{
-					Id("Name"):      Lit(svc.Name),
-					Id("RelPath"):   Lit(svc.Root.RelPath),
-					Id("Endpoints"): Nil(),
-				})
-			}
-		}),
-		Id("static").Op(":=").Op("&").Qual("encore.dev/runtime/config", "Static").Values(Dict{
-			Id("Services"):    Id("services"),
+	f.Comment("//go:linkname loadApp encore.dev/appruntime/app/appinit.load")
+	f.Func().Id("loadApp").Params().Op("*").Qual("encore.dev/appruntime/app/appinit", "LoadData").Block(
+		Id("static").Op(":=").Op("&").Qual("encore.dev/appruntime/config", "Static").Values(Dict{
 			Id("AuthData"):    b.authDataType(),
 			Id("Testing"):     True(),
 			Id("TestService"): Lit(testSvc),
 		}),
-		Return(Op("&").Qual("encore.dev/runtime/config", "Config").Values(Dict{
-			Id("Static"):  Id("static"),
-			Id("Runtime"): Qual("encore.dev/runtime/config", "ParseRuntime").Call(getEnv("ENCORE_RUNTIME_CONFIG")),
-			Id("Secrets"): Qual("encore.dev/runtime/config", "ParseSecrets").Call(getEnv("ENCORE_APP_SECRETS")),
-		}), Nil()),
+		Return(Op("&").Qual("encore.dev/appruntime/app/appinit", "LoadData").Values(Dict{
+			Id("StaticCfg"):   Id("static"),
+			Id("APIHandlers"): Nil(),
+		})),
 	)
 
 	return f
