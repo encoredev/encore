4068afaf6bfc7b2d449a0459b4f1d28c12f3bf8f
André Eriksson <andre@encore.dev>
2022-07-21 21:14:42 +0200

    runtime: delete old runtime

--- runtime/runtime/build_error.go
+++
@@ -1,8 +0,0 @@
-//go:build !encore_internal
-
-package runtime
-
-// this is a compile error which we've introduced on purpose to prevent accidental inclusion of the encore.dev/runtime package
-// outside of Encore applications.
-var encoreRuntimeIncludedByAccident = true
-var encoreRuntimeIncludedByAccident = true
