5dccf392ad839214ebcd92f786e545361634ad69
André Eriksson <andre@encore.dev>
2022-07-21 21:12:06 +0200

    compiler: use new codegen, simplify wrappers

--- compiler/wrappers.go
+++ compiler/wrappers.go
@@ -1,19 +1,23 @@
 package compiler
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
-	"encr.dev/compiler/internal/codegen"
 	"encr.dev/parser/est"
 )
 
-const mainPkgName = "__encore_main"
+const (
+	encorePkgDir = "__encore"
+	mainPkgName  = "main"
+	etypePkgName = "etype"
+)
 
 func (b *builder) writeMainPkg() error {
 	// Write the file to disk
-	dir := filepath.Join(b.workdir, mainPkgName)
-	if err := os.Mkdir(dir, 0755); err != nil {
+	dir := filepath.Join(b.workdir, encorePkgDir, mainPkgName)
+	if err := os.MkdirAll(dir, 0755); err != nil {
 		return err
 	}
 	mainPath := filepath.Join(dir, "main.go")
@@ -27,18 +31,23 @@ func (b *builder) writeMainPkg() error {
 		}
 	}()
 
-	b.addOverlay(filepath.Join(b.appRoot, mainPkgName, "main.go"), mainPath)
+	b.addOverlay(filepath.Join(b.appRoot, encorePkgDir, mainPkgName, "main.go"), mainPath)
 
-	mb := codegen.NewBuilder(b.res, b.cfg.EncoreCompilerVersion)
-	f, err := mb.Main()
+	f, err := b.codegen.Main()
 	if err != nil {
 		return err
 	}
 	return f.Render(file)
 }
 
-func (b *builder) generateWrappers(pkg *est.Package, rpcs []*est.RPC, wrapperPath string) (err error) {
-	file, err := os.Create(wrapperPath)
+func (b *builder) writeEtypePkg() error {
+	// Write the file to disk
+	dir := filepath.Join(b.workdir, encorePkgDir, etypePkgName)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	filePath := filepath.Join(dir, "etype.go")
+	file, err := os.Create(filePath)
 	if err != nil {
 		return err
 	}
@@ -48,8 +57,49 @@ func (b *builder) generateWrappers(pkg *est.Package, rpcs []*est.RPC, wrapperPat
 		}
 	}()
 
-	mb := codegen.NewBuilder(b.res, b.cfg.EncoreCompilerVersion)
-	f := mb.Wrappers(pkg, rpcs)
+	b.addOverlay(filepath.Join(b.appRoot, encorePkgDir, etypePkgName, "etype.go"), filePath)
+
+	f, err := b.codegen.Etype()
+	if err != nil {
+		return err
+	}
+	return f.Render(file)
+}
+
+func (b *builder) writeSvcHandlers() error {
+	for _, svc := range b.res.App.Services {
+		if err := b.writeHandlers(svc); err != nil {
+			return fmt.Errorf("write handlers for svc %s: %v", svc.Name, err)
+		}
+	}
+	return nil
+}
+
+func (b *builder) writeHandlers(svc *est.Service) error {
+	// Write the file to disk
+	dir := filepath.Join(b.workdir, filepath.FromSlash(svc.Root.RelPath))
+	name := "encore_internal__service.go"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+	filePath := filepath.Join(dir, name)
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err2 := file.Close(); err == nil {
+			err = err2
+		}
+	}()
+
+	b.addOverlay(filepath.Join(svc.Root.Dir, name), filePath)
+
+	f, err := b.codegen.ServiceHandlers(svc)
+	if err != nil {
+		return err
+	}
 	return f.Render(file)
 }
 
