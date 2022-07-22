dd81a82f3860ff61e552148e292335ef5defc62b
André Eriksson <andre@encore.dev>
2022-07-22 21:29:03 +0200

    Implement graceful shutdown for pubsub

--- runtime/pubsub/subscription.go
+++ runtime/pubsub/subscription.go
@@ -2,6 +2,8 @@ package pubsub
 
 import (
 	"context"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -19,6 +21,8 @@ import (
 )
 
 type Manager struct {
+	ctx        context.Context
+	cancelCtx  func()
 	cfg        *config.Config
 	rt         *reqtrack.RequestTracker
 	ts         *testsupport.Manager
@@ -27,12 +31,34 @@ type Manager struct {
 	nsq        *nsq.Manager
 
 	publishCounter uint64
+
+	outstanding *outstandingMessageTracker
 }
 
-func NewManager(appCtx context.Context, cfg *config.Config, rt *reqtrack.RequestTracker, ts *testsupport.Manager, server *api.Server, rootLogger zerolog.Logger) *Manager {
-	gcpMgr := gcp.NewManager(appCtx, cfg, server)
-	nsqMgr := nsq.NewManager(appCtx, cfg, rt)
-	return &Manager{cfg, rt, ts, rootLogger, gcpMgr, nsqMgr, 0}
+func NewManager(cfg *config.Config, rt *reqtrack.RequestTracker, ts *testsupport.Manager, server *api.Server, rootLogger zerolog.Logger) *Manager {
+	ctx, cancel := context.WithCancel(context.Background())
+	gcpMgr := gcp.NewManager(ctx, cfg, server)
+	nsqMgr := nsq.NewManager(ctx, cfg, rt)
+	return &Manager{
+		ctx:        ctx,
+		cancelCtx:  cancel,
+		cfg:        cfg,
+		rt:         rt,
+		ts:         ts,
+		rootLogger: rootLogger,
+		gcp:        gcpMgr,
+		nsq:        nsqMgr,
+	}
+}
+
+func (mgr *Manager) Shutdown(force context.Context) {
+	mgr.cancelCtx()
+	mgr.outstanding.ArmForShutdown()
+
+	select {
+	case <-mgr.outstanding.Done():
+	case <-force.Done():
+	}
 }
 
 // Subscription represents a subscription to a Topic.
@@ -117,6 +143,9 @@ func NewSubscription[T any](topic *Topic[T], name string, subscriptionCfg Subscr
 
 	// Subscribe to the topic
 	topic.topic.Subscribe(&log, subscriptionCfg.RetryPolicy, subscription, func(ctx context.Context, msgID string, publishTime time.Time, deliveryAttempt int, attrs map[string]string, data []byte) (err error) {
+		mgr.outstanding.Inc()
+		defer mgr.outstanding.Dec()
+
 		if !mgr.cfg.Static.Testing {
 			// Under test we're already inside an operation
 			mgr.rt.BeginOperation()
@@ -195,4 +224,61 @@ func (t *Topic[T]) getSubscriptionConfig(name string) (*config.PubsubSubscriptio
 	}
 
 	return subscription, staticCfg
+}
+
+// outstandingMessageTracker tracks the number of outstanding messages.
+// Once Shutdown() has been called, the next time the number of outstanding
+// messages reaches zero (or if it's already zero), the Done() channel is closed.
+type outstandingMessageTracker struct {
+	active int64
+
+	closeOnce sync.Once
+	done      chan struct{}
+
+	mu    sync.Mutex
+	armed bool
+}
+
+func newOutstandingMessageTracker() *outstandingMessageTracker {
+	return &outstandingMessageTracker{
+		done: make(chan struct{}),
+	}
+}
+
+func (t *outstandingMessageTracker) Inc() {
+	atomic.AddInt64(&t.active, 1)
+}
+
+func (t *outstandingMessageTracker) Dec() {
+	val := atomic.AddInt64(&t.active, -1)
+	if val < 0 {
+		panic("outstandingMessageTracker: active < 0")
+	}
+	if val == 0 {
+		t.mu.Lock()
+		defer t.mu.Unlock()
+		if t.armed {
+			t.markDone()
+		}
+	}
+}
+
+func (t *outstandingMessageTracker) ArmForShutdown() {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	t.armed = true
+
+	// If we're already at zero, mark it as done right away.
+	if atomic.LoadInt64(&t.active) == 0 {
+		t.markDone()
+	}
+}
+
+// markDone marks the tracker as being done.
+func (t *outstandingMessageTracker) markDone() {
+	t.closeOnce.Do(func() { close(t.done) })
+}
+
+func (t *outstandingMessageTracker) Done() <-chan struct{} {
+	return t.done
 }
