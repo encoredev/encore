94e80f52105d2598925b087f879efe1e93a85421
André Eriksson <andre@encore.dev>
2022-07-26 15:02:06 +0200

    Improve doc comments

---
+++ runtime/appruntime/doc.go
@@ -0,0 +1,6 @@
+// Package appruntime provides internal functionality for use by
+// Encore to run Encore applications. Packages within this directory
+// are intended exclusively for use by the Encore compiler and should
+// not be called directly from Encore applications. The APIs can change
+// at any time.
+package appruntime
