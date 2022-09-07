c44f672a7958f303ed80e8971297e9b454011cd2
Simon Johansson <simon@encore.dev>
2022-09-07 07:16:17 +0200

    Add functionality to system diagram visualisation (#358)
    * Change the layout algorithm to use elkjs
    * Hovering a node highlights it's descendants and shows dependencies
    * Refined the styling with Encore branding

--- cli/daemon/dash/dashapp/src/components/app/AppDiagram.test.tsx
+++ cli/daemon/dash/dashapp/src/components/app/AppDiagram.test.tsx
@@ -1,40 +1,221 @@
 import React from "react";
-import { render, screen, waitFor, within } from "@testing-library/react";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
 import AppDiagram from "~c/app/AppDiagram";
-import { META_DATA_FIXTURE } from "../../__tests__/meta-data-fixture";
+import { PositionedEdge, PositionedNode } from "~lib/graph/graph-utils";
+import * as ElkGraphLayoutData from "~lib/graph/elk-algo";
+
+jest.mock("@visx/responsive", () => ({
+  ParentSize: (props: any) => {
+    return props.children({
+      width: 1000,
+      height: 1000,
+    });
+  },
+}));
+
+const setMockLayoutData = (
+  nodes: Partial<PositionedNode>[],
+  edges: Partial<PositionedEdge> & { text: string }[] = []
+) => {
+  const mockCoordinates = { x: 0, y: 0 };
+  const mockSize = {
+    width: 100,
+    height: 100,
+  };
+  const mockNodes = nodes.map((node) => {
+    return {
+      id: node.label,
+      type: node.type || "service",
+      ...mockSize,
+      ...mockCoordinates,
+      ...node,
+    };
+  });
+  const mockEdges = edges.map((edge) => {
+    return {
+      id: (Math.random() + 1).toString(36).substring(7),
+      label: {
+        text: edge.text,
+        ...mockCoordinates,
+      },
+      points: [mockCoordinates],
+      ...edge,
+    };
+  });
+
+  jest
+    .spyOn(ElkGraphLayoutData, "getElkGraphLayoutData")
+    .mockImplementationOnce(
+      () => Promise.resolve({ nodes: mockNodes, edges: mockEdges }) as any
+    );
+};
+
+const getConnMock = (metaData: any = {}) => {
+  return {
+    request: () =>
+      Promise.resolve({
+        meta: { svcs: [], pubsub_topics: [], pkgs: [], ...metaData },
+      }),
+    on: () => {},
+    off: () => {},
+  } as any;
+};
 
 describe("AppDiagram", () => {
-  const connMock = {
-    request: () => Promise.resolve({ meta: META_DATA_FIXTURE }),
-  };
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+    cleanup();
+  });
 
-  it("should render nodes in diagram", async () => {
-    render(<AppDiagram appID={"app-id"} conn={connMock as any} />);
+  it("should render services and topics", async () => {
+    setMockLayoutData([
+      {
+        label: "service-1",
+      },
+      {
+        label: "topic-1",
+        type: "topic",
+      },
+    ]);
+    render(<AppDiagram appID={"app-id"} conn={getConnMock()} />);
 
     await waitFor(() => {
       expect(screen.getByText("service-1")).toBeInTheDocument();
-      expect(screen.getByText("service-2")).toBeInTheDocument();
-      expect(screen.getByText("service-3")).toBeInTheDocument();
       expect(screen.getByText("topic-1")).toBeInTheDocument();
     });
   });
 
-  it("should add database for service nodes", async () => {
-    render(<AppDiagram appID={"app-id"} conn={connMock as any} />);
+  it("should render edges and labels", async () => {
+    setMockLayoutData(
+      [
+        {
+          label: "service-1",
+        },
+        {
+          label: "service-2",
+        },
+      ],
+      [
+        {
+          type: "rpc",
+          source: "service-1",
+          target: "service-2",
+          text: "3",
+        },
+      ]
+    );
+    const { container } = render(
+      <AppDiagram appID={"app-id"} conn={getConnMock()} />
+    );
 
     await waitFor(() => {
-      const serviceNode1 = screen.getByText("service-1")
+      const edgeEl = container.querySelector(".edge");
+      expect(edgeEl).toBeTruthy();
+      expect(screen.getByText("3 RPCs")).toBeInTheDocument();
+    });
+  });
+
+  it("should show database for service", async () => {
+    setMockLayoutData([
+      {
+        label: "service-1",
+        has_database: true,
+      },
+    ]);
+    render(<AppDiagram appID={"app-id"} conn={getConnMock()} />);
+
+    await waitFor(() => {
+      const serviceNode1 = screen.getByText("service-1").parentElement!
         .parentElement as HTMLElement;
-      const serviceNode2 = screen.getByText("service-2")
+
+      expect(within(serviceNode1).getByText("Database")).toBeInTheDocument();
+    });
+  });
+
+  it("should show endpoints for service", async () => {
+    render(
+      <AppDiagram
+        appID={"app-id"}
+        conn={getConnMock({
+          svcs: [
+            {
+              name: "service-1",
+              rpcs: [
+                { access_type: "PUBLIC" },
+                { access_type: "AUTH" },
+                { access_type: "PRIVATE" },
+              ],
+              databases: [],
+            },
+          ],
+        })}
+      />
+    );
+
+    await waitFor(() => {
+      const serviceNode1 = screen.getByText("service-1").parentElement!
         .parentElement as HTMLElement;
 
       expect(
-        within(serviceNode1).getByTestId("service-database")
-      ).toBeInTheDocument();
-      expect(() =>
-        // Will throw because it can't find the element
-        within(serviceNode2).getByTestId("service-database")
-      ).toThrow();
+        within(serviceNode1).getByTestId("service-endpoints")
+      ).toHaveTextContent("1 public");
+      expect(
+        within(serviceNode1).getByTestId("service-endpoints")
+      ).toHaveTextContent("1 auth");
+      expect(
+        within(serviceNode1).getByTestId("service-endpoints")
+      ).toHaveTextContent("1 private");
+    });
+  });
+
+  it("should highlight descendants when hovering node", async () => {
+    setMockLayoutData(
+      [
+        {
+          label: "service-1",
+        },
+        {
+          label: "service-2",
+        },
+        {
+          label: "service-3",
+        },
+      ],
+      [
+        {
+          type: "rpc",
+          source: "service-1",
+          target: "service-2",
+          text: "1",
+        },
+      ]
+    );
+    const { container } = render(
+      <AppDiagram appID={"app-id"} conn={getConnMock()} />
+    );
+
+    await waitFor(() => {
+      fireEvent.mouseEnter(screen.getByTestId("node-service-1"));
+
+      // should now be visible
+      const label = container
+        .querySelector(".edge-group")!
+        .querySelector(".label");
+      expect(label!.classList).toContain("opacity-100");
+
+      // should be dimmed
+      expect(screen.getByTestId("node-service-3").classList).toContain(
+        "opacity-10"
+      );
     });
   });
 });
--- cli/daemon/dash/dashapp/src/components/app/AppDiagram.tsx
+++ cli/daemon/dash/dashapp/src/components/app/AppDiagram.tsx
@@ -1,23 +1,49 @@
 import React, { FC, useEffect, useState } from "react";
 import { APIMeta } from "~c/api/api";
-import JSONRPCConn from "~lib/client/jsonrpc";
-import { Group } from "@visx/group";
-import { Zoom } from "@visx/zoom";
+import JSONRPCConn, { NotificationMsg } from "~lib/client/jsonrpc";
 import {
   getEdgesFromMetaData,
   getNodesFromMetaData,
   GraphData,
+  GraphLayoutOptions,
+  NodeData,
   PositionedNode,
   ServiceNode,
 } from "~lib/graph/graph-utils";
-import { getGraphLayoutData } from "~lib/graph/dagre-algo";
+import { ProvidedZoom } from "@visx/zoom/lib/types";
+import { getElkGraphLayoutData } from "~lib/graph/elk-algo";
+import { ProcessReload } from "~lib/client/client";
+import { Group } from "@visx/group";
+import { Zoom } from "@visx/zoom";
 import {
+  EdgeLabelSVG,
   EdgeSVG,
   EncoreArrowHeadSVG,
   ServiceSVG,
   TopicSVG,
 } from "~c/svgElements";
-import { ProvidedZoom } from "@visx/zoom/lib/types";
+import { ParentSize } from "@visx/responsive";
+
+class AppDiagramLayoutOptions implements GraphLayoutOptions {
+  serviceNodeMinWidth = 220;
+  topicNodeMinWidth = 50;
+  serviceNodeMinHeight = 60;
+  topicNodeMinHeight = 40;
+
+  getNodeWidth(nodeData: NodeData) {
+    const labelLen = nodeData.label.length;
+    if (nodeData.type === "topic") {
+      return Math.max(this.topicNodeMinWidth, labelLen * 9 + 60);
+    }
+    return Math.max(this.serviceNodeMinWidth, labelLen * 12);
+  }
+
+  getNodeHeight(nodeData: NodeData) {
+    if (nodeData.type === "topic") return this.topicNodeMinHeight;
+    if (nodeData.has_database) return this.serviceNodeMinHeight + 27;
+    return this.serviceNodeMinHeight;
+  }
+}
 
 interface Props {
   appID: string;
@@ -27,25 +53,51 @@ interface Props {
 const AppDiagram: FC<Props> = ({ appID, conn }) => {
   const [metaData, setMetaData] = useState<APIMeta>();
   const [graphLayoutData, setGraphLayoutData] = useState<GraphData>();
-  const hasServiceDatabase = (node: ServiceNode) => {
+  const [activeNode, setActiveNode] = useState<PositionedNode | null>(null);
+  const [activeDescendants, setActiveDescendants] = useState<string[] | null>(
+    null
+  );
+  const [isInitialRender, setIsInitialRender] = useState<boolean>(true);
+  const graphWidth = graphLayoutData?.width ?? 1600;
+  const graphHeight = graphLayoutData?.height ?? 800;
+
+  const getNumberOfEndpoints = (node: ServiceNode) => {
     const svc = metaData!.svcs.find((s) => s.name === node.service_name);
-    return !!svc?.databases.some((dbName) => dbName === svc.name);
+    const endpoints = { public: 0, auth: 0, private: 0 };
+    svc?.rpcs.forEach((rpc) => {
+      if (rpc.access_type === "PUBLIC") endpoints.public++;
+      if (rpc.access_type === "AUTH") endpoints.auth++;
+      if (rpc.access_type === "PRIVATE") endpoints.private++;
+    });
+    return endpoints;
   };
-  const isServiceOutsideFacing = (node: ServiceNode) => {
-    const svc = metaData!.svcs.find((s) => s.name === node.service_name);
-    return !!svc?.rpcs.some(
-      (rpc) => rpc.access_type === "PUBLIC" || rpc.access_type === "AUTH"
-    );
+  const getDescendantNodes = (node: NodeData) => {
+    return graphLayoutData!.edges
+      .filter((e) => e.source === node.id)
+      .map((e) => e.target);
   };
-  const width = 1600;
-  const height = 800;
-  const initialZoomTransform = {
-    scaleX: 1,
-    scaleY: 1,
-    translateX: 20,
-    translateY: 20,
-    skewX: 0,
-    skewY: 0,
+  const isNodeActive = (node: NodeData) =>
+    activeNode === null ||
+    activeNode.id === node.id ||
+    !!activeDescendants?.includes(node.id);
+  const onNodeClick = (
+    zoom: ProvidedZoom<SVGSVGElement>,
+    node: PositionedNode
+  ) => {
+    const centerPoint = { x: graphWidth / 2, y: graphHeight / 2 };
+    const inverseCentroid = zoom.applyInverseToPoint(centerPoint);
+    zoom.translate({
+      translateX: inverseCentroid.x - node.x,
+      translateY: inverseCentroid.y - node.y,
+    });
+  };
+  const onNodeMouseEnter = (node: PositionedNode) => {
+    setActiveNode(node);
+    setActiveDescendants(getDescendantNodes(node));
+  };
+  const onNodeMouseLeave = () => {
+    setActiveNode(null);
+    setActiveDescendants(null);
   };
 
   useEffect(() => {
@@ -54,126 +106,165 @@ const AppDiagram: FC<Props> = ({ appID, conn }) => {
         setMetaData(status.meta);
       }
     });
+    const onNotify = (msg: NotificationMsg) => {
+      if (msg.method === "process/reload") {
+        const data = msg.params as ProcessReload;
+        if (data.appID === appID) {
+          setMetaData(data.meta);
+        }
+      }
+    };
+    conn.on("notification", onNotify);
+    return () => {
+      conn.off("notification", onNotify);
+    };
   }, []);
 
   useEffect(() => {
-    const nodeMinWidth = 150;
-    const serviceNodeHeight = 80;
-    const topicNodeHeight = 40;
     if (metaData) {
-      const graphLayoutData = getGraphLayoutData(
+      getElkGraphLayoutData(
         getNodesFromMetaData(metaData),
         getEdgesFromMetaData(metaData),
-        {
-          getNodeWidth: (nodeData) => {
-            const labelLen = nodeData.label.length;
-            return Math.max(nodeMinWidth, labelLen * 12);
-          },
-          getNodeHeight: (nodeData) => {
-            return nodeData.type === "topic"
-              ? topicNodeHeight
-              : serviceNodeHeight;
-          },
-          // drawOutsideDependencyToNode: (nodeData) => {
-          //   return nodeData.type === "service" && isServiceOutsideFacing(nodeData);
-          // },
-        }
-      );
-      setGraphLayoutData(graphLayoutData);
+        new AppDiagramLayoutOptions()
+      ).then(setGraphLayoutData);
     }
   }, [metaData]);
-
-  const getNodeElement = (
-    zoom: ProvidedZoom<SVGSVGElement>,
-    node: PositionedNode
-  ) => {
-    const onNodeClick = () => {
-      const centerPoint = { x: width / 2, y: height / 2 };
-      const inverseCentroid = zoom.applyInverseToPoint(centerPoint);
-      zoom.translate({
-        translateX: inverseCentroid.x - node.x,
-        translateY: inverseCentroid.y - node.y,
-      });
-    };
-    switch (node.type) {
-      case "service":
-        return (
-          <ServiceSVG
-            key={node.id}
-            node={node}
-            hasDatabase={hasServiceDatabase(node)}
-            isOutsideFacing={isServiceOutsideFacing(node)}
-            onClick={onNodeClick}
-          />
-        );
-      case "topic":
-        return <TopicSVG key={node.id} node={node} onClick={onNodeClick} />;
-      default:
-        return null;
-    }
-  };
 
   if (!graphLayoutData) return null;
 
   return (
-    <div className="flex flex-col">
-      <Zoom<SVGSVGElement>
-        width={width}
-        height={height}
-        scaleXMin={0.5}
-        scaleXMax={4}
-        scaleYMin={0.5}
-        scaleYMax={4}
-        initialTransformMatrix={initialZoomTransform}
-      >
-        {(zoom) => (
-          <div className="relative">
-            <svg
-              width={width}
-              height={height}
-              style={{
-                cursor: zoom.isDragging ? "grabbing" : "grab",
-                touchAction: "none",
-              }}
-              ref={zoom.containerRef}
-            >
-              <defs>
-                <EncoreArrowHeadSVG id="encore-arrow" fill="#333" />
-              </defs>
+    <div
+      className="relative flex w-full flex-col items-center justify-center"
+      style={{ height: "calc(100vh - 120px)", background: "#EEEEE1" }}
+    >
+      {!graphLayoutData.nodes.length ? (
+        <div>
+          <p>Add an encore service to your app and it will show up here</p>
+        </div>
+      ) : (
+        <ParentSize>
+          {(parent) => {
+            useEffect(() => {
+              if (parent.width && parent.height) {
+                setIsInitialRender(false);
+              }
+            }, [parent]);
+            if (!parent.width || !parent.height) return null;
 
-              {/* Background */}
-              <rect
-                width={width}
-                height={height}
-                fill="#FFF"
-                onTouchStart={zoom.dragStart}
-                onTouchMove={zoom.dragMove}
-                onTouchEnd={zoom.dragEnd}
-                onMouseDown={zoom.dragStart}
-                onMouseMove={zoom.dragMove}
-                onMouseUp={zoom.dragEnd}
-                onMouseLeave={() => {
-                  if (zoom.isDragging) zoom.dragEnd();
+            return (
+              <Zoom<SVGSVGElement>
+                width={parent.width}
+                height={parent.height}
+                scaleXMin={0.5}
+                scaleXMax={4}
+                scaleYMin={0.5}
+                scaleYMax={4}
+                initialTransformMatrix={{
+                  scaleX: 1,
+                  scaleY: 1,
+                  translateX: parent.width / 2 - graphLayoutData?.width! / 2,
+                  translateY: parent.height / 2 - graphLayoutData?.height! / 2,
+                  skewX: 0,
+                  skewY: 0,
                 }}
-              />
-
-              {/* Drawable area */}
-              <Group
-                transform={zoom.toString()}
-                // style={{ transition: "transform 0.2s ease-out" }}
               >
-                {graphLayoutData.nodes.map(getNodeElement.bind(null, zoom))}
-                {graphLayoutData.edges.map((edge, index) => (
-                  <EdgeSVG
-                    key={`${edge.source}-${edge.target}-${index}`}
-                    edge={edge}
-                  />
-                ))}
-              </Group>
-            </svg>
-          </div>
-        )}
-      </Zoom>
+                {(zoom) => (
+                  <div className="relative">
+                    <svg
+                      className="app-diagram"
+                      width={parent.width}
+                      height={parent.height}
+                      ref={zoom.containerRef}
+                    >
+                      <defs>
+                        <EncoreArrowHeadSVG id="encore-arrow" fill="#333" />
+                      </defs>
+
+                      {/* Background */}
+                      <rect
+                        fill="#EEEEE1"
+                        width={parent.width}
+                        height={parent.height}
+                        onTouchStart={zoom.dragStart}
+                        onTouchMove={zoom.dragMove}
+                        onTouchEnd={zoom.dragEnd}
+                        onMouseDown={zoom.dragStart}
+                        onMouseMove={zoom.dragMove}
+                        onMouseUp={zoom.dragEnd}
+                        onMouseLeave={() => {
+                          if (zoom.isDragging) zoom.dragEnd();
+                        }}
+                        style={{
+                          cursor: zoom.isDragging ? "grabbing" : "grab",
+                          touchAction: "none",
+                        }}
+                      />
+
+                      {/* Drawable area */}
+                      <Group transform={zoom.toString()}>
+                        {graphLayoutData.edges.map((edge) => (
+                          <Group key={edge.id} className="edge-group">
+                            <EdgeSVG
+                              edge={edge}
+                              activeNodeId={activeNode?.id || null}
+                              isInitialRender={isInitialRender}
+                            />
+                            <EdgeLabelSVG
+                              edge={edge}
+                              activeNodeId={activeNode?.id || null}
+                            />
+                          </Group>
+                        ))}
+                        {graphLayoutData.nodes.map((node) => {
+                          if (node.type === "service")
+                            return (
+                              <ServiceSVG
+                                key={node.id}
+                                node={node}
+                                isActive={isNodeActive(node)}
+                                endpoints={getNumberOfEndpoints(node)}
+                                onClick={onNodeClick.bind(null, zoom, node)}
+                                onMouseEnter={onNodeMouseEnter.bind(null, node)}
+                                onMouseLeave={onNodeMouseLeave.bind(null)}
+                              />
+                            );
+                          if (node.type === "topic") {
+                            return (
+                              <TopicSVG
+                                key={node.id}
+                                node={node}
+                                isActive={isNodeActive(node)}
+                                onClick={onNodeClick.bind(null, zoom, node)}
+                                onMouseEnter={onNodeMouseEnter.bind(null, node)}
+                                onMouseLeave={onNodeMouseLeave.bind(null)}
+                              />
+                            );
+                          }
+                        })}
+                      </Group>
+                    </svg>
+                  </div>
+                )}
+              </Zoom>
+            );
+          }}
+        </ParentSize>
+      )}
+      <a
+        target="_blank"
+        href="https://encoredev.slack.com/app_redirect?channel=CQFNUESN9"
+      >
+        <p
+          className="absolute left-2 bottom-2 p-2 text-sm font-semibold"
+          style={{ background: "#EEEEE1" }}
+        >
+          Want to see more?
+          <br />
+          <span className="font-normal underline">
+            Please share your feedback and ideas
+          </span>
+        </p>
+      </a>
     </div>
   );
 };
