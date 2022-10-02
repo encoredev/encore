46c1625845fb05b1576a8d0525e58df78a66386e
Stefan Ekerfelt <ekerfelt@gmail.com>
2022-10-02 22:31:29 +0200

    Fix pointer

--- cli/internal/conf/conf.go
+++ cli/internal/conf/conf.go
@@ -167,7 +167,7 @@ func (ts *TokenSource) Token() (*oauth2.Token, error) {
 	})
 	if err != nil {
 		var re *oauth2.RetrieveError
-		if errors.As(err, re) && re.Response.StatusCode == 422 {
+		if errors.As(err, &re) && re.Response.StatusCode == 422 {
 			// The refresh token is invalid. Log the user out to reset the token.
 			_ = Logout()
 		}
