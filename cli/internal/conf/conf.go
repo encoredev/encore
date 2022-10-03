28fce782473ba5700906cd80604a29b9bc18e3be
Stefan Ekerfelt <stefan@encore.dev>
2022-10-03 14:47:27 +0200

    Stop daemon and parse the correct error
    The credentials are cached on the daemon. Easiest to just stop any running instance

--- cli/internal/conf/conf.go
+++ cli/internal/conf/conf.go
@@ -166,11 +166,6 @@ func (ts *TokenSource) Token() (*oauth2.Token, error) {
 		return nil
 	})
 	if err != nil {
-		var re *oauth2.RetrieveError
-		if errors.As(err, &re) && re.Response.StatusCode == 422 {
-			// The refresh token is invalid. Log the user out to reset the token.
-			_ = Logout()
-		}
 		return nil, err
 	}
 	token, err := ts.ts.Token()
