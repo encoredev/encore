babf7cfaf21df4425410417c8d264ead2c978788
Dominic Black <dom@encore.dev>
2022-10-17 10:53:57 +0000

    Pass computed Config to Tests (#445)
    This commit adds support to `encore test` for the new config system, and
    will pass the computed config into any tests, allowing them to opperate
    off those tests.

--- pkg/errinsrc/stack_dev.go
+++ pkg/errinsrc/stack_dev.go
@@ -2,4 +2,6 @@
 
 package errinsrc
 
-var includeStackByDefault = true
+// IncludeStackByDefault is whether to include the stack by default on all errors.
+// This is exported to allow Encore's CI platform to set this to true for CI/CD builds.
+var IncludeStackByDefault = true
