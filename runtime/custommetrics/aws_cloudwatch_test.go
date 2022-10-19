895eeefadef6b593fedde14cee0500b497a5bf8c
Luca Campese <luca@encore.dev>
2022-10-19 18:33:30 +0200

    Add test

---
+++ runtime/custommetrics/aws_cloudwatch_test.go
@@ -0,0 +1,25 @@
+package custommetrics
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/rs/zerolog"
+)
+
+func TestCounter_AWS(t *testing.T) {
+	var buf bytes.Buffer
+	logger := zerolog.New(&buf)
+	awsMetricsManager := awsMetricsManager{
+		metricPrefix: "app_a1b2",
+		logger:       logger,
+	}
+
+	awsMetricsManager.Counter("test_counter", map[string]string{"key": "value"})
+	actual := buf.String()
+	want := `{"level":"trace","e_metric_name":"app_a1b2_test_counter","key":"value"}
+`
+	if actual != want {
+		t.Fatalf("want %q, got %q", want, actual)
+	}
+}
