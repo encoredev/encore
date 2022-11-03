2da8e34db2c1765032a655e173f60bcf2977f237
André Eriksson <andre@encore.dev>
2022-11-03 01:09:47 +0100

    runtime: add cloud provider build tags
    This adds cloud provider-specific build tags
    that allow us to conditionally disable specific
    providers from being part of the build,
    avoiding their huge SDK packages from being
    imported unnecessarily.

---
+++ runtime/pubsub/provider_nsq.go
@@ -0,0 +1,13 @@
+//go:build !encore_no_local
+
+package pubsub
+
+import (
+	"encore.dev/pubsub/internal/nsq"
+)
+
+func init() {
+	registerProvider(func(mgr *Manager) provider {
+		return nsq.NewManager(mgr.ctx, mgr.cfg, mgr.rt)
+	})
+}
