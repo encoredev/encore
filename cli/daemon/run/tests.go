975ac75616e899253742fb9f5f4a71d4ce3522de
André Eriksson <andre@encore.dev>
2022-11-04 17:37:27 +0100

    cli/daemon: split handlers into separate files

--- cli/daemon/run/tests.go
+++ cli/daemon/run/tests.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"os"
 	"strconv"
 	"time"
 
@@ -16,60 +15,12 @@ import (
 	"encore.dev/appruntime/config"
 	"encr.dev/cli/daemon/apps"
 	"encr.dev/cli/daemon/sqldb"
-	"encr.dev/cli/internal/appfile"
 	"encr.dev/compiler"
 	"encr.dev/internal/env"
-	"encr.dev/internal/experiments"
 	"encr.dev/internal/version"
 	"encr.dev/parser"
 	"encr.dev/pkg/cueutil"
-	"encr.dev/pkg/vcs"
 )
-
-// Check checks the app for errors.
-// It reports a buildDir (if available) when codegenDebug is true.
-func (mgr *Manager) Check(ctx context.Context, appRoot, relwd string, codegenDebug bool) (buildDir string, err error) {
-	vcsRevision := vcs.GetRevision(appRoot)
-
-	exp, err := appfile.Experiments(appRoot)
-	if err != nil {
-		return "", err
-	}
-	expSet, err := experiments.NewSet(exp, nil)
-	if err != nil {
-		return "", err
-	}
-
-	// TODO: We should check that all secret keys are defined as well.
-	cfg := &compiler.Config{
-		Revision:              vcsRevision.Revision,
-		UncommittedChanges:    vcsRevision.Uncommitted,
-		WorkingDir:            relwd,
-		CgoEnabled:            true,
-		EncoreCompilerVersion: fmt.Sprintf("EncoreCLI/%s", version.Version),
-		EncoreRuntimePath:     env.EncoreRuntimePath(),
-		EncoreGoRoot:          env.EncoreGoRoot(),
-		KeepOutput:            codegenDebug,
-		BuildTags:             []string{"encore_local", "encore_no_gcp", "encore_no_aws", "encore_no_azure"},
-		Experiments:           expSet,
-		Meta: &cueutil.Meta{
-			// Dummy data to satisfy config validation.
-			APIBaseURL: "http://localhost:0",
-			EnvName:    "encore-check",
-			EnvType:    cueutil.EnvType_Development,
-			CloudType:  cueutil.CloudType_Local,
-		},
-	}
-	result, err := compiler.Build(appRoot, cfg)
-	if result != nil && result.Dir != "" {
-		if codegenDebug {
-			buildDir = result.Dir
-		} else {
-			os.RemoveAll(result.Dir)
-		}
-	}
-	return buildDir, err
-}
 
 // TestParams groups the parameters for the Test method.
 type TestParams struct {
---
+++ cli/daemon/test.go
@@ -0,0 +1,107 @@
+package daemon
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog/log"
+
+	"encr.dev/cli/daemon/run"
+	"encr.dev/cli/daemon/sqldb"
+	"encr.dev/cli/internal/appfile"
+	daemonpb "encr.dev/proto/encore/daemon"
+)
+
+// Test runs tests.
+func (s *Server) Test(req *daemonpb.TestRequest, stream daemonpb.Daemon_TestServer) error {
+	slog := &streamLog{stream: stream, buffered: false}
+	sendErr := func(err error) {
+		slog.Stderr(false).Write([]byte(err.Error() + "\n"))
+		streamExit(stream, 1)
+	}
+
+	// Prefetch secrets if the app is linked.
+	if appSlug, err := appfile.Slug(req.AppRoot); err == nil && appSlug != "" {
+		s.sm.Prefetch(appSlug)
+	}
+
+	// Parse the app to figure out what infrastructure is needed.
+	parse, err := s.parseApp(req.AppRoot, req.WorkingDir, true /* parse tests */)
+	if err != nil {
+		sendErr(err)
+		return nil
+	}
+
+	app, err := s.apps.Track(req.AppRoot)
+	if err != nil {
+		sendErr(err)
+		return nil
+	}
+
+	// Set up the database asynchronously since it can take a while.
+	dbSetupErr := make(chan error, 1)
+
+	var cluster *sqldb.Cluster
+	if sqldb.IsUsed(parse.Meta) {
+		setupCtx, setupCancel := context.WithTimeout(context.Background(), 30*time.Second)
+		cluster = s.cm.Create(setupCtx, &sqldb.CreateParams{
+			ClusterID: sqldb.GetClusterID(app, sqldb.Test),
+			Memfs:     true,
+		})
+
+		go func() {
+			defer setupCancel()
+			if _, err := cluster.Start(setupCtx); err != nil {
+				dbSetupErr <- err
+			} else if err := cluster.Recreate(setupCtx, req.AppRoot, nil, parse.Meta); err != nil {
+				dbSetupErr <- err
+			}
+		}()
+	}
+
+	testCtx, cancel := context.WithCancel(stream.Context())
+	defer cancel()
+
+	testResults := make(chan error, 1)
+	go func() {
+		defer func() {
+			if recovered := recover(); recovered != nil {
+				var err error
+				switch recovered := recovered.(type) {
+				case error:
+					err = recovered
+				default:
+					err = fmt.Errorf("%v", recovered)
+				}
+				log.Err(err).Msg("panic during test run")
+				testResults <- fmt.Errorf("panic occured within Encore during test run: %v\n", recovered)
+			}
+		}()
+
+		tp := run.TestParams{
+			App:          app,
+			SQLDBCluster: cluster,
+			WorkingDir:   req.WorkingDir,
+			Environ:      req.Environ,
+			Args:         req.Args,
+			Parse:        parse,
+			Stdout:       slog.Stdout(false),
+			Stderr:       slog.Stderr(false),
+		}
+		testResults <- s.mgr.Test(testCtx, tp)
+	}()
+
+	select {
+	case err := <-dbSetupErr:
+		sendErr(err)
+		return nil
+	case err := <-testResults:
+		if err != nil {
+			sendErr(err)
+		} else {
+			streamExit(stream, 0)
+		}
+		return nil
+	}
+}
