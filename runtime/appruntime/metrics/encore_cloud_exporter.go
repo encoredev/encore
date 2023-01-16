5e687b6a8a94bec57c334b7733b9fd8c398b7741
Luca Campese <luca@encore.dev>
2023-01-16 15:58:21 +0100

    Add Cloud Run instance ID to node_id

--- runtime/appruntime/metrics/encore_cloud_exporter.go
+++ runtime/appruntime/metrics/encore_cloud_exporter.go
@@ -3,6 +3,8 @@
 package metrics
 
 import (
+	"cloud.google.com/go/compute/metadata"
+
 	"encore.dev/appruntime/config"
 	"encore.dev/appruntime/metrics/gcp"
 )
@@ -14,7 +16,14 @@ func init() {
 			return cfg.EncoreCloud != nil
 		},
 		newExporter: func(mgr *Manager) exporter {
+			cloudRunInstanceID, err := metadata.InstanceID()
+			if err != nil {
+				mgr.rootLogger.Err(err).Msg("unable to initialize metrics exporter")
+				return nil
+			}
+
 			metricsCfg := mgr.cfg.Runtime.Metrics
+			metricsCfg.EncoreCloud.MonitoredResourceLabels["node_id"] += "-" + cloudRunInstanceID
 			return gcp.New(mgr.cfg.Static.BundledServices, metricsCfg.EncoreCloud, mgr.rootLogger)
 		},
 	})
