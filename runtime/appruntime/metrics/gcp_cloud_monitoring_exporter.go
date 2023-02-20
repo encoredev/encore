3aed04321dbbd7672abe6f67ba43192fa767a5a4
Luca Campese <luca@encore.dev>
2023-02-20 16:12:28 +0100

    Track system metrics (#613)

--- runtime/appruntime/metrics/gcp_cloud_monitoring_exporter.go
+++ runtime/appruntime/metrics/gcp_cloud_monitoring_exporter.go
@@ -3,9 +3,8 @@
 package metrics
 
 import (
-	"cloud.google.com/go/compute/metadata"
-
 	"encore.dev/appruntime/config"
+	"encore.dev/appruntime/metadata"
 	"encore.dev/appruntime/metrics/gcp"
 )
 
@@ -16,24 +15,20 @@ func init() {
 			return cfg.CloudMonitoring != nil
 		},
 		newExporter: func(mgr *Manager) exporter {
-			cloudRunInstanceID, err := metadata.InstanceID()
+			containerMetadata, err := metadata.GetContainerMetadata(mgr.cfg.Runtime)
 			if err != nil {
-				mgr.rootLogger.Err(err).Msg(
-					"unable to initialize metrics exporter: error getting Cloud Run instance ID",
-				)
+				mgr.rootLogger.Err(err).Msg("unable to initialize metrics exporter: error getting container metadata")
 				return nil
 			}
 
 			metricsCfg := mgr.cfg.Runtime.Metrics
 			nodeID, ok := metricsCfg.CloudMonitoring.MonitoredResourceLabels["node_id"]
 			if !ok {
-				mgr.rootLogger.Err(err).Msg(
-					"unable to initialize metrics exporter: missing node_id",
-				)
+				mgr.rootLogger.Err(err).Msg("unable to initialize metrics exporter: missing node_id")
 				return nil
 			}
-			metricsCfg.CloudMonitoring.MonitoredResourceLabels["node_id"] = nodeID + "-" + cloudRunInstanceID
-			return gcp.New(mgr.cfg.Static.BundledServices, metricsCfg.CloudMonitoring, mgr.rootLogger)
+			metricsCfg.CloudMonitoring.MonitoredResourceLabels["node_id"] = nodeID + "-" + containerMetadata.InstanceID
+			return gcp.New(mgr.cfg.Static.BundledServices, metricsCfg.CloudMonitoring, containerMetadata, mgr.rootLogger)
 		},
 	})
 }
