encoredev/encore

commit 2fe470bfbb308be3b53b45528f02f7660a7593ad

Improve parser performance (#505)
This improves Encore's parser performance across the board with several
changes:
* Concurrent parsing of directories
* Conditional compilation of cloud provider libraries for smaller build
lists
* Better detection of which resources are possibly in use for a given
package and file, based on imports
* And a few more minor changes

parser/parser.go
@@ -9,9 +9,10 @@ import (
 	goparser "go/parser"
 	"go/scanner"
 	"go/token"
-	"os"
+	"io/fs"
 	"path"
 	"path/filepath"
+	"runtime"
 	"sort"
 	"strings"
 
@@ -145,6 +146,7 @@ func (p *parser) Parse() (res *Result, err error) {
 	for pkgPath, name := range defaultTrackedPackages {
 		track[pkgPath] = name
 	}
+
 	p.resolveNames(track)
 	p.parseServices()
 	p.parseResources()
@@ -176,6 +178,7 @@ func (p *parser) Parse() (res *Result, err error) {
 		AuthHandler:   p.authHandler,
 		Middleware:    p.middleware,
 	}
+
 	md, nodes, err := ParseMeta(p.cfg.AppRevision, p.cfg.AppHasUncommittedChanges, p.cfg.AppRoot, app, p.fset)
 	if err != nil {
 		return nil, err
@@ -202,10 +205,10 @@ func encoreBuildContext() build.Context {
 
 // collectPackages collects and parses the regular Go AST
 // for all subdirectories in the root.
-func collectPackages(fs *token.FileSet, rootDir, rootImportPath string, mode goparser.Mode, parseTests bool) ([]*est.Package, error) {
+func collectPackages(fset *token.FileSet, rootDir, rootImportPath string, mode goparser.Mode, parseTests bool) ([]*est.Package, error) {
 	var pkgs []*est.Package
 	var errors scanner.ErrorList
-	filter := func(f os.FileInfo) bool {
+	filter := func(f fs.DirEntry) bool {
 		// Don't parse encore.gen.go files, since they're not intended to be checked in.
 		// We've had several issues where things work locally but not in CI/CD because
 		// the encore.gen.go file was parsed for local development which papered over issues.
@@ -218,16 +221,10 @@ func collectPackages(fs *token.FileSet, rootDir, rootImportPath string, mode gop
 
 	buildContext := encoreBuildContext()
 
-	err := walkDirs(rootDir, func(dir, relPath string, files []os.FileInfo) error {
-		ps, pkgFiles, err := parseDir(buildContext, fs, dir, relPath, filter, mode)
+	parsePkg := func(dir, relPath string, files []fs.DirEntry) (*est.Package, error) {
+		ps, pkgFiles, err := parseDir(buildContext, fset, dir, files, filter, mode)
 		if err != nil {
-			// If the error is an error list, it means we have a parsing error.
-			// Keep going with other directories in that case.
-			if el, ok := err.(scanner.ErrorList); ok {
-				errors = append(errors, el...)
-				return nil
-			}
-			return err
+			return nil, err
 		}
 
 		var pkgNames []string
@@ -242,12 +239,12 @@ func collectPackages(fs *token.FileSet, rootDir, rootImportPath string, mode gop
 				// It's just a "_test" package; we're good.
 			} else {
 				namestr := strings.Join(pkgNames[:n-1], ", ") + " and " + pkgNames[n-1]
-				errors.Add(fs.Position(first.Pos()), "got multiple package names in directory: "+namestr)
-				return nil
+				errors.Add(fset.Position(first.Pos()), "got multiple package names in directory: "+namestr)
+				return nil, nil
 			}
 		} else if n == 0 {
 			// No Go files; ignore directory
-			return nil
+			return nil, nil
 		}
 
 		p := ps[pkgNames[0]]
@@ -256,7 +253,7 @@ func collectPackages(fs *token.FileSet, rootDir, rootImportPath string, mode gop
 		for _, astFile := range p.Files {
 			// HACK: getting package comments is not at all easy
 			// because of the quirks of go/ast. This seems to work.
-			cm := ast.NewCommentMap(fs, astFile, astFile.Comments)
+			cm := ast.NewCommentMap(fset, astFile, astFile.Comments)
 			for _, cg := range cm[astFile] {
 				if text := strings.TrimSpace(cg.Text()); text != "" {
 					doc = text
@@ -276,16 +273,87 @@ func collectPackages(fs *token.FileSet, rootDir, rootImportPath string, mode gop
 			RelPath:    path.Clean(relPath),
 			Dir:        dir,
 			Files:      pkgFiles,
+			Imports:    make(map[string]bool),
 		}
 		for _, f := range pkgFiles {
 			f.Pkg = pkg
+
+			for importPath := range f.Imports {
+				pkg.Imports[importPath] = true
+			}
 		}
-		pkgs = append(pkgs, pkg)
-		return nil
-	})
-	if err != nil {
-		return nil, err
+		return pkg, nil
 	}
+
+	type dirToParse struct {
+		dir     string
+		relPath string
+		files   []fs.DirEntry
+	}
+
+	numWorkers := runtime.GOMAXPROCS(0)
+	if numWorkers < 4 {
+		numWorkers = 4
+	}
+
+	work := make(chan dirToParse, 100)
+	pkgCh := make(chan *est.Package, numWorkers)
+	errCh := make(chan error, numWorkers)
+	quit := make(chan struct{})
+	workerDone := make(chan struct{}, numWorkers)
+
+	worker := func() {
+		defer func() { workerDone <- struct{}{} }()
+		for d := range work {
+			pkg, err := parsePkg(d.dir, d.relPath, d.files)
+			if err != nil {
+				errCh <- err
+				return
+			}
+			if pkg != nil {
+				pkgCh <- pkg
+			}
+		}
+	}
+
+	for i := 0; i < numWorkers; i++ {
+		go worker()
+	}
+
+	go func() {
+		err := walkDirs(rootDir, func(dir, relPath string, files []fs.DirEntry) error {
+			work <- dirToParse{dir: dir, relPath: relPath, files: files}
+			return nil
+		})
+		close(work) // no more work
+		if err != nil {
+			errCh <- err
+		}
+	}()
+
+	defer close(quit)
+	numWorkersDone := 0
+	for numWorkersDone < numWorkers {
+		select {
+		case pkg := <-pkgCh:
+			pkgs = append(pkgs, pkg)
+		case err := <-errCh:
+			// If the error is an error list, it means we have a parsing error.
+			// Keep going in that case.
+			if el, ok := err.(scanner.ErrorList); ok {
+				errors = append(errors, el...)
+			} else {
+				return nil, err
+			}
+
+		case <-workerDone:
+			numWorkersDone++
+		}
+	}
+
+	sort.Slice(pkgs, func(i, j int) bool {
+		return pkgs[i].RelPath < pkgs[j].RelPath
+	})
 	return pkgs, errors.Err()
 }
 

commit 883088b641fe2bec89a0d3e623913a6994f44f6b

[ENC-1288] Fix indeterministic build/encore run
This commit fixes a bug introduced in #505 when we made the parser run in parallel.

The issue ultimately was a race condition within the main goroutine, such that when the last worker finished parsing a package, the main goroutine could either read from the `pkgCh` or the `workerDone` channel.

If it picked the `workerDone` channel, then the last package would never be added to the list of known packages, which could result in random errors later in the encore parse/compile pipeline.

I've also updated the unit tests to ensure the test cache is cleared for each PR

.github/workflows/go.yml
@@ -50,7 +50,7 @@ jobs:
       run: cd encr.dev && go build ./...
 
     - name: Test
-      run: cd encr.dev && go test -short ./...
+      run: cd encr.dev && go test ./...
       env:
         ENCORE_GOROOT: ${{ github.workspace }}/encore-go
 

.gitignore
@@ -17,6 +17,7 @@ cli/daemon/dash/dashapp/public/assets/fonts/*/**
 # JetBrains
 .idea
 .fleet
+.run
 
 # MacOS
 .DS_Store

parser/parser.go
@@ -15,6 +15,7 @@ import (
 	"runtime"
 	"sort"
 	"strings"
+	"sync"
 
 	"golang.org/x/exp/slices"
 	"golang.org/x/tools/go/ast/astutil"
@@ -313,30 +314,15 @@ func collectPackages(fset *token.FileSet, rootDir, rootImportPath, mainPkgRelPat
 		numWorkers = 4
 	}
 
+	type result struct {
+		pkg *est.Package
+		err error
+	}
+
 	work := make(chan dirToParse, 100)
-	pkgCh := make(chan *est.Package, numWorkers)
-	errCh := make(chan error, numWorkers)
-	quit := make(chan struct{})
-	workerDone := make(chan struct{}, numWorkers)
+	results := make(chan result, numWorkers)
 
-	worker := func() {
-		defer func() { workerDone <- struct{}{} }()
-		for d := range work {
-			pkg, err := parsePkg(d.dir, d.relPath, d.files)
-			if err != nil {
-				errCh <- err
-				return
-			}
-			if pkg != nil {
-				pkgCh <- pkg
-			}
-		}
-	}
-
-	for i := 0; i < numWorkers; i++ {
-		go worker()
-	}
-
+	// Enqueue all the directories to parse
 	go func() {
 		err := walkDirs(rootDir, func(dir, relPath string, files []fs.DirEntry) error {
 			work <- dirToParse{dir: dir, relPath: relPath, files: files}
@@ -344,27 +330,46 @@ func collectPackages(fset *token.FileSet, rootDir, rootImportPath, mainPkgRelPat
 		})
 		close(work) // no more work
 		if err != nil {
-			errCh <- err
+			results <- result{err: err}
 		}
 	}()
 
-	defer close(quit)
-	numWorkersDone := 0
-	for numWorkersDone < numWorkers {
-		select {
-		case pkg := <-pkgCh:
-			pkgs = append(pkgs, pkg)
-		case err := <-errCh:
+	// Start the workers
+	var activeWorkers sync.WaitGroup
+	worker := func() {
+		defer activeWorkers.Done()
+		for d := range work {
+			pkg, err := parsePkg(d.dir, d.relPath, d.files)
+			results <- result{pkg: pkg, err: err}
+			if err != nil {
+				return
+			}
+		}
+	}
+	for i := 0; i < numWorkers; i++ {
+		activeWorkers.Add(1)
+		go worker()
+	}
+
+	// When all the workers are done, close the results channel
+	go func() {
+		defer close(results)
+		activeWorkers.Wait()
+	}()
+
+	// Collect the results
+	for result := range results {
+		switch {
+		case result.pkg != nil:
+			pkgs = append(pkgs, result.pkg)
+		case result.err != nil:
 			// If the error is an error list, it means we have a parsing error.
 			// Keep going in that case.
-			if el, ok := err.(scanner.ErrorList); ok {
+			if el, ok := result.err.(scanner.ErrorList); ok {
 				errors = append(errors, el...)
 			} else {
-				return nil, err
+				return nil, result.err
 			}
-
-		case <-workerDone:
-			numWorkersDone++
 		}
 	}
 