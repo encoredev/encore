a308b9c8a5dcbde7a3d70eaa366b4281b712f73e
André Eriksson <andre@encore.dev>
2023-04-19 11:42:28 +0200

    Fix golden files

--- v2/codegen/apigen/maingen/maingen_test.go
+++ v2/codegen/apigen/maingen/maingen_test.go
@@ -6,11 +6,13 @@ import (
 	"encr.dev/v2/app"
 	"encr.dev/v2/codegen"
 	"encr.dev/v2/codegen/apigen"
+	"encr.dev/v2/codegen/apigen/maingen"
 	"encr.dev/v2/codegen/internal/codegentest"
 	"encr.dev/v2/internals/pkginfo"
 )
 
 func TestCodegen(t *testing.T) {
+	maingen.GenerateForInternalPackageTests = true
 	fn := func(gen *codegen.Generator, desc *app.Desc) {
 		loader := pkginfo.New(gen.Context)
 		mainModule := loader.MainModule()
@@ -18,7 +20,7 @@ func TestCodegen(t *testing.T) {
 			Gen:           gen,
 			Desc:          desc,
 			MainModule:    mainModule,
-			RuntimeModule: loader.MainModule(),
+			RuntimeModule: loader.RuntimeModule(),
 		}
 		apigen.Process(params)
 	}
