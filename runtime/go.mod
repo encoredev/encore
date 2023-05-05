93c94aca51e5993625004870f349fdaba6723e3a
Dominic Black <dom@encore.dev>
2023-05-05 16:02:02 +0100

    Add Exactly Once docs
    This commits adds the documentation for Exactly Once semantics on pubsub topics

--- runtime/go.mod
+++ runtime/go.mod
@@ -29,8 +29,9 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/nsqio/go-nsq v1.1.0
 	github.com/rs/cors v1.8.3-0.20221003140808-fcebdb403f4d
+	github.com/rs/xid v1.4.0
 	github.com/rs/zerolog v1.29.1
-	go.encore.dev/platform-sdk v0.0.0-20230428112231-2e173f37d8fd
+	go.encore.dev/platform-sdk v1.0.0
 	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53
 	golang.org/x/time v0.3.0
 	google.golang.org/api v0.119.0
@@ -72,7 +73,7 @@ require (
 	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
@@ -82,7 +83,7 @@ require (
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/oauth2 v0.7.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
--- runtime/pubsub/internal/types/public.go
+++ runtime/pubsub/internal/types/public.go
@@ -49,11 +49,38 @@ type DeliveryGuarantee int
 
 const (
 	// AtLeastOnce guarantees that a message for a subscription is delivered to
-	// a consumer at least once. This is supported by all cloud providers.
+	// a consumer at least once.
+	//
+	// On AWS and GCP there is no limit to the throughput for a topic.
 	AtLeastOnce DeliveryGuarantee = iota + 1
 
 	// ExactlyOnce guarantees that a message for a subscription is delivered to
-	// a consumer exactly once
+	// a consumer exactly once, to the best of the system's ability.
+	//
+	// However, there are edge cases when a message might be redelivered.
+	// For example, if a networking issue causes the acknowledgement of success
+	// processing the message to be lost before the cloud provider receives it.
+	//
+	// It is also important to note that the ExactlyOnce delivery guarantee only
+	// applies to the delivery of the message to the consumer, and not to the
+	// original publishing of the message, such that if a message is published twice,
+	// such as due to an retry within the application logic, it will be delivered twice.
+	// (i.e. ExactlyOnce delivery does not imply message deduplication on publish)
+	//
+	// As such it's recommended that the subscription handler function is idempotent
+	// and is able to handle duplicate messages.
+	//
+	// ExactlyOnce delivery topics have significantly higher latency for subscriptions
+	// compared to AtLeastOnce subscriptions.
+	//
+	// By using ExactlyOnce semantics on a topic, the throughput of will be limited
+	//
+	// - AWS: 300 messages per second for the topic (see [AWS SQS Quotas]).
+	// - GCP: At least 3,000 messages per second across all topics in the region
+	// 		  (can be higher on the region see [GCP PubSub Quotas]).
+	//
+	// [AWS SQS Quotas]: https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/quotas-messages.html
+	// [GCP PubSub Quotas]: https://cloud.google.com/pubsub/quotas#quotas
 	ExactlyOnce
 )
 
