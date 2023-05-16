0ff111ff88d8f31f99245c27b2ac7e23592a4aac
Phakorn Kiong <phakorn214@gmail.com>
2023-05-16 13:24:55 +0000

    fix tests

--- internal/clientgen/testdata/expected_noauth_typescript.ts
+++ internal/clientgen/testdata/expected_noauth_typescript.ts
@@ -109,6 +109,9 @@ type CallParameters = Omit<RequestInit, "method" | "body"> & {
 
     /** Any query parameters to be sent with the request */
     query?: Record<string, string | string[]>
+
+    /** indicates whether the user agent should send or receive cookies from the other domain in the case of cross-origin requests. */
+    credentials?: RequestCredentials;
 }
 
 
