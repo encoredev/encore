4166520c2d1c95eff3fdad51af9e7a449eab5a8a
André Eriksson <andre@encore.dev>
2023-05-18 22:32:20 +0200

    cli: notify dashboard of new traces

--- cli/daemon/engine/trace2/store.go
+++ cli/daemon/engine/trace2/store.go
@@ -72,5 +72,5 @@ type Store interface {
 	Get(ctx context.Context, appID, traceID string, iter EventIterator) error
 
 	// Listen listens for new spans.
-	Listen(ch <-chan *tracepb2.SpanSummary)
+	Listen(ch chan<- *tracepb2.SpanSummary)
 }
