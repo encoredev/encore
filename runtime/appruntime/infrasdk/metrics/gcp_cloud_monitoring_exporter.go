1483716d749fb195a9bbcfb3d31d35aea5d98488
Dominic Black <dom@encore.dev>
2023-07-26 19:41:28 +0100

    Support GKE in GCP Cloud Monitoring (#816)
    This commit adds support for us to deploy into GKE and emit metrics as a
    [K8S
    container](https://cloud.google.com/monitoring/api/resources#tag_k8s_container)

--- runtime/appruntime/infrasdk/metrics/gcp_cloud_monitoring_exporter.go
+++ runtime/appruntime/infrasdk/metrics/gcp_cloud_monitoring_exporter.go
@@ -3,6 +3,9 @@
 package metrics
 
 import (
+	"os"
+	"strings"
+
 	"encore.dev/appruntime/exported/config"
 	"encore.dev/appruntime/infrasdk/metadata"
 	"encore.dev/appruntime/infrasdk/metrics/gcp"
@@ -22,12 +25,19 @@ func init() {
 			}
 
 			metricsCfg := mgr.runtime.Metrics
+
 			nodeID, ok := metricsCfg.CloudMonitoring.MonitoredResourceLabels["node_id"]
-			if !ok {
-				mgr.rootLogger.Err(err).Msg("unable to initialize metrics exporter: missing node_id")
-				return nil
+			if ok {
+				metricsCfg.CloudMonitoring.MonitoredResourceLabels["node_id"] = nodeID + "-" + containerMetadata.InstanceID
 			}
-			metricsCfg.CloudMonitoring.MonitoredResourceLabels["node_id"] = nodeID + "-" + containerMetadata.InstanceID
+
+			// Replace $ENV: prefix with the actual environment variable value
+			for k, v := range metricsCfg.CloudMonitoring.MonitoredResourceLabels {
+				if strings.HasPrefix(v, "$ENV:") {
+					metricsCfg.CloudMonitoring.MonitoredResourceLabels[k] = os.Getenv(v[5:])
+				}
+			}
+
 			return gcp.New(mgr.static.BundledServices, metricsCfg.CloudMonitoring, containerMetadata, mgr.rootLogger)
 		},
 	})
