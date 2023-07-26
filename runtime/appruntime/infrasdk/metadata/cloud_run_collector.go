47994691446de400ede193241e191e6a15507570
Dominic Black <dom@encore.dev>
2023-07-26 19:33:32 +0100

    Support GKE in GCP Cloud Monitoring
    This commit adds support for us to deploy into GKE and emit metrics as a [K8S container](https://cloud.google.com/monitoring/api/resources#tag_k8s_container)

--- runtime/appruntime/infrasdk/metadata/cloud_run_collector.go
+++ runtime/appruntime/infrasdk/metadata/cloud_run_collector.go
@@ -30,15 +30,31 @@ func init() {
 			}
 			revision = strings.TrimPrefix(revision, service+"-")
 
-			instanceID, err := gcemetadata.InstanceID()
-			if err != nil {
-				return nil, fmt.Errorf("unable to get instance ID: %w", err)
+			instanceID, ok := os.LookupEnv("K_POD")
+			if !ok {
+				// If we don't have a K8s POD name, we're running on Cloud Run and can get the instance ID from the metadata server
+				var err error
+				instanceID, err = gcemetadata.InstanceID()
+				if err != nil {
+					return nil, fmt.Errorf("unable to get instance ID: %w", err)
+				}
+				instanceID = instanceID[len(instanceID)-8:]
+			} else {
+				// If we have a K8s POD name, take the last part of it which is the random pod ID
+				// On GKE, the InstanceID appears to be the Node, so if the multiple replicas are running
+				// on the same InstanceID then we'd have a collision. This is unlikely, but possible -
+				// hence why we use the pod ID instead.
+				idx := strings.LastIndex(instanceID, "-")
+				if idx == -1 {
+					return nil, fmt.Errorf("invalid instance ID '%s'", instanceID)
+				}
+				instanceID = instanceID[idx+1:]
 			}
 
 			return &ContainerMetadata{
 				ServiceID:  service,
 				RevisionID: revision,
-				InstanceID: instanceID[len(instanceID)-8:],
+				InstanceID: instanceID,
 			}, nil
 		},
 	})
