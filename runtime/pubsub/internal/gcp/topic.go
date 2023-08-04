e38b6effb1b9eabf3664f55ca3576bb8738fc0b3
Dominic Black <dom@encore.dev>
2023-08-04 15:06:13 +0100

    Move our GCP PubSub subscribers to use go routines for processing (#832)
    We've noticed for ExactlyOnce subscriptions with a high MaxConcurrency
    number, that the GCP PubSub library fails to pass any messages to our
    recieve function. By dropping the number of goroutines we allow the GCP
    library to use, we can prevent these problems and recieve messages - so
    we've set this always to 10
    
    To maintain the max concurrency limit and allow Encore apps to achieve
    the throughput they desire, we then push messages into new goroutines
    for each message, relying on the GCP Pub/Sub library to obey the
    `MaxConcurrency` number
    
    For anybody else who encounters this, this appeared as a error in our
    logs of:
    > automatic lease modack retry failed for following IDs
    
    Upon investigation and adding more logging to the GCP Pub/Sub library we
    discovered the source error was this:
    > modack failed: rpc error: code = DeadlineExceeded desc = context
    deadline exceeded
    
    Which we've put down to resource starvation due to the number of running
    goroutines.

--- runtime/pubsub/internal/gcp/topic.go
+++ runtime/pubsub/internal/gcp/topic.go
@@ -90,52 +90,58 @@ func (t *topic) Subscribe(logger *zerolog.Logger, maxConcurrency int, ackDeadlin
 		// Create the subscription object (and then check it exists on GCP's side)
 		subscription := t.mgr.getClientForProject(gcpCfg.ProjectID).Subscription(subCfg.ProviderName)
 
+		// Set the concurrency
 		if maxConcurrency == 0 {
 			maxConcurrency = 1000 // FIXME(domblack): This retains the old behaviour, but allows user customisation - in a future release we should remove this
 		}
-
-		// Set the concurrency
 		subscription.ReceiveSettings.MaxOutstandingMessages = maxConcurrency
-		subscription.ReceiveSettings.NumGoroutines = utils.Clamp(maxConcurrency, 1, maxConcurrency)
 
-		// Start the subscription
+		// We've seen resource starvation with large numbers of goroutines, so we limit the number of goroutines to 10
+		// and then spawn a new goroutine for each message received, we rely on the MaxOutstandingMessages and GCP Pub/Sub
+		// library to limit the number of messages we receive at once
+		subscription.ReceiveSettings.NumGoroutines = 10
+
+		// Start the subscription with the GCP library
 		go func() {
 			for t.mgr.ctxs.Fetch.Err() == nil {
 				// Subscribe to the topic to receive messages
 				err := subscription.Receive(t.mgr.ctxs.Fetch, func(_ context.Context, msg *pubsub.Message) {
-					deliveryAttempt := 1
-					if msg.DeliveryAttempt != nil {
-						deliveryAttempt = *msg.DeliveryAttempt
-					}
-
-					// Create a context from the handler context with a deadline of the ackdeadline
-					ctx, cancel := context.WithTimeout(t.mgr.ctxs.Handler, ackDeadline)
-					defer cancel()
-
-					var result *pubsub.AckResult
-					if err := f(ctx, msg.ID, msg.PublishTime, deliveryAttempt, msg.Attributes, msg.Data); err != nil {
-						result = msg.NackWithResult()
-					} else {
-						result = msg.AckWithResult()
-					}
-
-					res, err := result.Get(t.mgr.ctxs.Connection)
-					if err != nil {
-						logger.Warn().Err(err).Str("msg_id", msg.ID).Msg("failed to ack/nack message")
-					} else {
-						switch res {
-						case pubsub.AcknowledgeStatusSuccess:
-							return
-						case pubsub.AcknowledgeStatusPermissionDenied:
-							logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to permissions")
-						case pubsub.AcknowledgeStatusFailedPrecondition:
-							logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to precondition")
-						case pubsub.AcknowledgeStatusInvalidAckID:
-							logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to invalid ack ID")
-						default:
-							logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to unknown error")
+					// run each message in a separate goroutine to allow the subscription goroutine to continue to fetch
+					// messages from GCP
+					go func() {
+						deliveryAttempt := 1
+						if msg.DeliveryAttempt != nil {
+							deliveryAttempt = *msg.DeliveryAttempt
 						}
-					}
+
+						// Create a context from the handler context with a deadline of the ackdeadline
+						ctx, cancel := context.WithTimeout(t.mgr.ctxs.Handler, ackDeadline)
+						defer cancel()
+
+						var result *pubsub.AckResult
+						if err := f(ctx, msg.ID, msg.PublishTime, deliveryAttempt, msg.Attributes, msg.Data); err != nil {
+							result = msg.NackWithResult()
+						} else {
+							result = msg.AckWithResult()
+						}
+
+						res, err := result.Get(t.mgr.ctxs.Connection)
+						if err != nil {
+							logger.Warn().Err(err).Str("msg_id", msg.ID).Msg("failed to ack/nack message")
+						} else {
+							switch res {
+							case pubsub.AcknowledgeStatusSuccess:
+							case pubsub.AcknowledgeStatusPermissionDenied:
+								logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to permissions")
+							case pubsub.AcknowledgeStatusFailedPrecondition:
+								logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to precondition")
+							case pubsub.AcknowledgeStatusInvalidAckID:
+								logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to invalid ack ID")
+							default:
+								logger.Error().Str("msg_id", msg.ID).Msg("failed to ack/nack message due to unknown error")
+							}
+						}
+					}()
 				})
 
 				// If there was an error and we're not shutting down, log it and then sleep for a bit before trying again
