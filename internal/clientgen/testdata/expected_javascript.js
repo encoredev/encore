d466ede0448a652ca9c2faa0da1da7918ebc1542
Anish Chakraborty <anish749@users.noreply.github.com>
2024-01-21 14:36:47 +0000

    fix test data

--- internal/clientgen/testdata/expected_javascript.js
+++ internal/clientgen/testdata/expected_javascript.js
@@ -281,7 +281,7 @@ class BaseClient {
             if (typeof auth === "function") {
                 this.authGenerator = auth
             } else {
-                this.authGenerator = () => auth                
+                this.authGenerator = () => Promise.resolve(auth)
             }
         }
 
@@ -303,7 +303,7 @@ class BaseClient {
         // If authorization data generator is present, call it and add the returned data to the request
         let authData
         if (this.authGenerator) {
-            authData = this.authGenerator()
+            authData = await this.authGenerator()
         }
 
         // If we now have authentication data, add it to the request
