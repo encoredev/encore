afd4db694ba654b760fd27a94ee85e6cc608e119
André Eriksson <andre@encore.dev>
2024-03-11 11:14:36 +0100

    rtconfgen: fix UnsafeAllowOriginsWithCredentials

--- pkg/rtconfgen/convert.go
+++ pkg/rtconfgen/convert.go
@@ -110,7 +110,7 @@ func (c *legacyConverter) Convert() (*config.Runtime, error) {
 					case *runtimev1.Gateway_CORS_AllowedOrigins:
 						allowOriginsWithCredentials = data.AllowedOrigins.AllowedOrigins
 					case *runtimev1.Gateway_CORS_UnsafeAllowAllOriginsWithCredentials:
-						allowOriginsWithCredentials = []string{"*"}
+						allowOriginsWithCredentials = []string{config.UnsafeAllOriginWithCredentials}
 					}
 					cfg.CORS = &config.CORS{
 						Debug:                          gw.Cors.Debug,
