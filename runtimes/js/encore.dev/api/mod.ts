79c81a256b433ef63f47f97bb46362739aa7990a
André Eriksson <andre@encore.dev>
2024-03-26 14:19:10 +0100

    runtimes/js: move `APIError` to `api` package. (#1106)

--- runtimes/js/encore.dev/api/mod.ts
+++ runtimes/js/encore.dev/api/mod.ts
@@ -13,12 +13,12 @@ export type Method =
 
 export type Header<
   TypeOrName extends string | number | boolean = string,
-  Name extends string = "",
+  Name extends string = ""
 > = TypeOrName extends string ? string : TypeOrName;
 
 export type Query<
   TypeOrName extends string | number | boolean = string,
-  Name extends string = "",
+  Name extends string = ""
 > = TypeOrName extends string ? string : TypeOrName;
 
 export interface APIOptions {
@@ -34,19 +34,20 @@ export interface APIOptions {
 
 export type Handler<
   Params extends object | void = void,
-  Response extends object | void = void,
+  Response extends object | void = void
 > = Params extends void
   ? () => Promise<Response>
   : (params: Params) => Promise<Response>;
 
 export function api<
   Params extends object | void = void,
-  Response extends object | void = void,
+  Response extends object | void = void
 >(
   options: APIOptions,
-  fn: Handler<Params, Response>,
+  fn: Handler<Params, Response>
 ): Handler<Params, Response> {
   return fn;
 }
 
+export { APIError, ErrCode } from "./error";
 export { Gateway, type GatewayConfig } from "./gateway";
