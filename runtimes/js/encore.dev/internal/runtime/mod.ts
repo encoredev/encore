8e862e39c98d0831f68ed87e7e2c21178f7e444f
Fredrik Enestad <fredrik@enestad.com>
2024-08-22 14:50:14 +0100

    runtimes/js: don't set worker mode in tests (#1332)
    Variable `isMainThread` is `false` in tests, causing the runtime to
    setup incorrectly.

--- runtimes/js/encore.dev/internal/runtime/mod.ts
+++ runtimes/js/encore.dev/internal/runtime/mod.ts
@@ -3,7 +3,9 @@ import { Runtime } from "./napi/napi.cjs";
 
 export * from "./napi/napi.cjs";
 
+const testMode = process.env.NODE_ENV === "test";
+
 export const RT = new Runtime({
-  testMode: process.env.NODE_ENV === "test",
-  isWorker: !isMainThread,
+  testMode,
+  isWorker: !isMainThread && !testMode,
 });
