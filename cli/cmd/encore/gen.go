08e5fb0cf1a02985f91e70a34a7bf2695a66c79e
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2024-09-03 11:22:42 +0200

    fix: increase timeout for api client generation

--- cli/cmd/encore/gen.go
+++ cli/cmd/encore/gen.go
@@ -75,7 +75,7 @@ To further narrow down the services to generate, use the '--services' flag.
 				lang = string(l)
 			}
 
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 			defer cancel()
 
 			daemon := setupDaemon(ctx)
