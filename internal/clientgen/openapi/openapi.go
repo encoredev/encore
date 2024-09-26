f23ca12bcec2a0f225ec0e34b16ad4bd626fc400
André Eriksson <eandre@gmail.com>
2024-09-26 16:12:51 +0200

    Revert "Only include non-private endpoints in OpenAPI spec output"
    This reverts commit 3a5ecf768b18e8ac11d37d0d55e90447f477abd4.

--- internal/clientgen/openapi/openapi.go
+++ internal/clientgen/openapi/openapi.go
@@ -83,11 +83,6 @@ func (g *Generator) addService(svc *meta.Service) error {
 			continue
 		}
 
-		// Skip non-public RPCs
-		if rpc.AccessType == meta.RPC_PRIVATE {
-			continue
-		}
-
 		if err := g.addRPC(rpc); err != nil {
 			return err
 		}
