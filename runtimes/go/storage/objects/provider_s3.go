9a2444f32eb95521208e5ab596e36aa9a172100b
Stefan Ekerfelt <ekerfelt@gmail.com>
2024-11-20 13:01:36 +0100

    object_storage: Fix GO s3 provider registration (#1586)

--- runtimes/go/storage/objects/provider_s3.go
+++ runtimes/go/storage/objects/provider_s3.go
@@ -6,11 +6,11 @@ import (
 	"context"
 
 	"encore.dev/appruntime/exported/config"
-	"encore.dev/storage/objects/internal/providers/gcs"
+	"encore.dev/storage/objects/internal/providers/s3"
 )
 
 func init() {
 	registerProvider(func(ctx context.Context, runtimeCfg *config.Runtime) provider {
-		return gcs.NewManager(ctx, runtimeCfg)
+		return s3.NewManager(ctx, runtimeCfg)
 	})
 }
