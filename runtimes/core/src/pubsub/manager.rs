fc1b4cfa1262bfb295c8fd4ea60bc6c018c75023
Fredrik Enestad <fredrik@enestad.com>
2024-12-05 15:40:31 +0100

    set status and pass data to handler

--- runtimes/core/src/pubsub/manager.rs
+++ runtimes/core/src/pubsub/manager.rs
@@ -184,6 +184,7 @@ impl SubHandler {
                     payload: msg.data.raw_body.clone(),
                     parsed_payload,
                 }),
+                middleware_meta: None.into(),
             });
 
             let logger = crate::log::root();
