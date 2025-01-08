ed7bbab225bc65537a157b41baccd863392b4b79
Fredrik Enestad <fredrik@enestad.com>
2025-01-08 14:53:57 +0100

    make marker optional

--- runtimes/js/encore.dev/validate/mod.ts
+++ runtimes/js/encore.dev/validate/mod.ts
@@ -1,55 +1,55 @@
 declare const __validate: unique symbol;
 
 export type Min<N extends number> = {
-  [__validate]: {
+  [__validate]?: {
     minValue: N;
   };
 };
 
 export type Max<N extends number> = {
-  [__validate]: {
+  [__validate]?: {
     maxValue: N;
   };
 };
 
 export type MinLen<N extends number> = {
-  [__validate]: {
+  [__validate]?: {
     minLen: N;
   };
 };
 
 export type MaxLen<N extends number> = {
-  [__validate]: {
+  [__validate]?: {
     maxLen: N;
   };
 };
 
 export type MatchesRegexp<S extends string> = {
-  [__validate]: {
+  [__validate]?: {
     matchesRegexp: S;
   };
 };
 
 export type StartsWith<S extends string> = {
-  [__validate]: {
+  [__validate]?: {
     startsWith: S;
   };
 };
 
 export type EndsWith<S extends string> = {
-  [__validate]: {
+  [__validate]?: {
     endsWith: S;
   };
 };
 
 export type IsEmail = {
-  [__validate]: {
+  [__validate]?: {
     isEmail: true;
   };
 };
 
 export type IsURL = {
-  [__validate]: {
+  [__validate]?: {
     isURL: true;
   };
 };
