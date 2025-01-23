8184e3dbde320190724d40774c0b62db54d61f66
Fredrik Enestad <fredrik@enestad.com>
2025-01-23 10:52:27 +0100

    clientgen/{ts,js}: default content type on raw request (#1717)
    node-fetch will automatically figure out the content type (such as
    `FormData`) and handle it for certain types, see
    https://fetch.spec.whatwg.org/#concept-bodyinit-extract and it seems
    like relying on that logic for raw endoints would be a sane default

--- internal/clientgen/testdata/tsapp/expected_stream_javascript.js
+++ internal/clientgen/testdata/tsapp/expected_stream_javascript.js
@@ -319,9 +319,7 @@ const boundFetch = fetch.bind(this)
 class BaseClient {
     constructor(baseURL, options) {
         this.baseURL = baseURL
-        this.headers = {
-            "Content-Type": "application/json",
-        }
+        this.headers = {}
 
         // Add User-Agent header if the script is running in the server
         // because browsers do not allow setting User-Agent headers to requests
@@ -404,6 +402,15 @@ class BaseClient {
 
         const queryString = query ? '?' + encodeQuery(query) : ''
         return new StreamOut(this.baseURL + path + queryString, headers);
+    }
+
+
+    // callTypedAPI makes an API call, defaulting content type to "application/json"
+    async callTypedAPI(method, path, body, params) {
+        return this.callAPI(method, path, body, {
+            ...params,
+            headers: { "Content-Type": "application/json", ...params?.headers }
+        });
     }
 
     // callAPI is used by each generated API method to actually make the request
