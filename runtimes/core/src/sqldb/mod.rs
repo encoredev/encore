ed77335a71dfb25bc21c5c1235b00b1c9bf27709
Fredrik Enestad <fredrik@enestad.com>
2025-04-09 09:32:30 +0200

    database transaction support in typescript (#1847)
    Adds support for transactions in runtimes/core and runtimes/js
    
    We should probably merge
    https://github.com/encoredev/rust-postgres/pull/1 and update Cargo.toml
    again. (suggested that change upstream as well
    https://github.com/sfackler/rust-postgres/pull/1230)

--- runtimes/core/src/sqldb/mod.rs
+++ runtimes/core/src/sqldb/mod.rs
@@ -1,7 +1,9 @@
 mod client;
 mod manager;
+mod transaction;
 mod val;
 
 pub use client::{Connection, Cursor, Pool, Row};
 pub use manager::{Database, DatabaseImpl, Manager, ManagerConfig};
+pub use transaction::Transaction;
 pub use val::RowValue;
