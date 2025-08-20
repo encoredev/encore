07e011ddabf4629fb15c9bad1fd9a1bbba5e4238
Fredrik Enestad <fredrik@enestad.com>
2025-08-20 15:08:31 +0200

    re-add accidentally removed test

--- tsparser/src/parser/types/testdata/wirespec.ts
+++ tsparser/src/parser/types/testdata/wirespec.ts
@@ -1,4 +1,12 @@
-import { HttpStatus } from "encore.dev/api";
+import { Query, Header, HttpStatus } from "encore.dev/api";
+import { MinLen, Max } from "encore.dev/validate";
+
+export interface Foo {
+  a: Query<number>;
+  b: Query<number> & Max<10>;
+  c: Query<string> & MinLen<3>;
+  d: Header<"X-Header"> & MinLen<3>;
+}
 
 export interface ResponseWithStatus {
   data: string;
