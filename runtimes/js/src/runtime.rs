0fe6f3efc1ba3c861274c2c439abcde015a060a2
Fredrik Enestad <fredrik@enestad.com>
2025-10-02 22:02:21 +0200

    get or init

--- runtimes/js/src/runtime.rs
+++ runtimes/js/src/runtime.rs
@@ -103,12 +103,11 @@ impl Runtime {
             .get_or_init(|| Ok(Arc::new(init_runtime(false)?)))
             .clone()?;
 
-        if TYPE_CONSTRUCTORS.get().is_none() {
-            let refs = TypeConstructorRefs {
-                decimal: env.create_reference(options.type_constructors.decimal)?,
-            };
-            let _ = TYPE_CONSTRUCTORS.set(refs);
-        }
+        TYPE_CONSTRUCTORS.get_or_init(|| TypeConstructorRefs {
+            decimal: env
+                .create_reference(options.type_constructors.decimal)
+                .expect("couldn't create reference to Decimal"),
+        });
 
         Ok(Self { runtime })
     }
